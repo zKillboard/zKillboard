e8c019d812fade7c50197e718bf9c2d774d84f91
Clinton Weiss <cvweiss@gmail.com>
2015-06-02 14:41:11 +0000

    new ranks code, not quite finished yet but commiting anyway

--- view/ranks.php
+++ view/ranks.php
@@ -1,21 +1,30 @@
 <?php
-exit("These rank pages have been temporarily disabled");
+
+global $mdb;
 
 if (!in_array($pageType, array("recent", "alltime"))) $app->notFound();
 if (!in_array($subType, array("killers", "losers"))) $app->notFound();
 
-$table = $pageType == "recent" ? "zz_ranks_recent" : "zz_ranks";
+$rankType = $pageType == "recent" ? "recentOverallRank" : "overallRank";
 $pageTitle = $pageType == "recent" ? "Ranks - Recent (Past 90 Days)" : "Alltime Ranks";
 $tableTitle = $pageType == "recent" ? "Recent Rank" : "Alltime Rank";
-
-$rankColumns = $subType == "killers" ? "(sdRank <= 10 or pdRank <= 10 or idRank <= 10 or overallRank <= 10)" : "(slRank <= 10 or plRank <= 10 or ilRank <= 10)";
 
 $types = array("pilot" => "characterID", "corp" => "corporationID", "alli" => "allianceID", "faction" => "factionID");
 $names = array("character" => "Characters", "corp" => "Corporations", "alli" => "Alliances", "faction" => "Factions");
 $ranks = array();
 foreach ($types as $type=>$column) {
-	$result = Db::query("select distinct typeID $column, r.* from $table r where type = '$type' and $rankColumns order by overallRank");
+	$r = $mdb->find("statistics", ['cacheTime' => 7200, 'type' => $column, $rankType => ['$ne' => null]], [$rankType => 1], 10);
+	$result = [];
+	foreach ($r as $row)
+	{
+		unset($row["groups"]);
+		unset($row["months"]);
+		unset($row["topAllTime"]);
+		$row[$column] = $row['id'];
+		$result[] = $row;
+	}
 	if ($type == "pilot") $type = "character";
+	Info::addInfo($result);
 	$ranks[] = array("type" => $type, "data" => $result, "name" => $names[$type]);
 }
 
