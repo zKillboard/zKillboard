da1f872a4d6234a7cf3fc2bcb77e6afea11e30e1
Clinton Weiss <cvweiss@gmail.com>
2015-06-10 05:07:17 +0000

    removed scrape check code

--- cron/everyMinute.php
+++ cron/everyMinute.php
@@ -10,8 +10,6 @@ Db::execute("delete from zz_users_sessions where validTill < now()");
 // Keep the account balance table clean
 Db::execute("delete from zz_account_balance where balance = 0");
 
-Db::execute("delete from zz_scrape_prevention where dttm < date_sub(now(), interval 1 hour)");
-
 // Cleanup subdomain stuff
 Db::execute("update zz_subdomains set adfreeUntil = null where adfreeUntil < now()");
 Db::execute("update zz_subdomains set banner = null where banner = ''");
