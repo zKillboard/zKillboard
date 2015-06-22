bc98ad506e838dea8670afefe301b1b348726cbb
Clinton Weiss <cvweiss@gmail.com>
2015-06-22 23:50:12 +0000

    some tweaks to the code

--- cron/0.apiProducer.php
+++ cron/0.apiProducer.php
@@ -38,62 +38,64 @@ $requestNum = 0;
 while ($timer->stop() <= 58000)
 {
 	$row = $tqApis->next();
-	if ($row == null) continue;
-
-	$keyID = $row["keyID"];
-	$vCode = $row["vCode"];
-
-	if (!isset($row["characters"])) $row["characters"] = [];
-
-	$errorCode = (int) @$row["errorCode"];
-	if ($errorCode == 0 || $errorCode == 221)
+	if ($row !== null) 
 	{
-		\Pheal\Core\Config::getInstance()->http_user_agent = "API Fetcher for http://$baseAddr";
-		\Pheal\Core\Config::getInstance()->http_post = false;
-		\Pheal\Core\Config::getInstance()->http_keepalive = true; // default 15 seconds
-		\Pheal\Core\Config::getInstance()->http_keepalive = 10; // KeepAliveTimeout in seconds
-		\Pheal\Core\Config::getInstance()->http_timeout = 30;
-		\Pheal\Core\Config::getInstance()->api_customkeys = true;
 
-		$pheal = new \Pheal\Pheal($keyID, $vCode);
-		try
+		$keyID = $row["keyID"];
+		$vCode = $row["vCode"];
+
+		if (!isset($row["characters"])) $row["characters"] = [];
+
+		$errorCode = (int) @$row["errorCode"];
+		if ($errorCode == 0 || $errorCode == 221)
 		{
-			$apiKeyInfo = $pheal->ApiKeyInfo();
-		} catch (Exception $ex)
-		{
-			$errorCode = (int) $ex->getCode();
-			if ($errorCode == 904) { Util::out("(apiProducer) 904'ed"); exit(); }
-			if ($errorCode == 28)
+			\Pheal\Core\Config::getInstance()->http_user_agent = "API Fetcher for http://$baseAddr";
+			\Pheal\Core\Config::getInstance()->http_post = false;
+			\Pheal\Core\Config::getInstance()->http_keepalive = true; // default 15 seconds
+			\Pheal\Core\Config::getInstance()->http_keepalive = 10; // KeepAliveTimeout in seconds
+			\Pheal\Core\Config::getInstance()->http_timeout = 30;
+			\Pheal\Core\Config::getInstance()->api_customkeys = true;
+
+			$pheal = new \Pheal\Pheal($keyID, $vCode);
+			try
 			{
-				Util::out("(apiProducer) API Server timeout");
-				exit();
-			}
-			$tqApis->remove($row); // Problem with api the key, remove it from rotation
-			if ($errorCode != 221 && $debug) Util::out("(apiProducer) Error Validating $keyID: " . $ex->getCode() . " " . $ex->getMessage());
-			$apis->update(['keyID' => $keyID, 'vCode' => $vCode], ['$set' => ['errorCode' => $errorCode]]);
-			continue;
-		}    
-
-		$key = @$apiKeyInfo->key;
-		$accessMask = @$key->accessMask;
-		$characterIDs = array();
-		if ($accessMask & 256)
-		{
-			foreach ($apiKeyInfo->key->characters as $character)
+				$apiKeyInfo = $pheal->ApiKeyInfo();
+			} catch (Exception $ex)
 			{
-				$characterID = (int) $character->characterID;
-				if (!isset($row["characters"][$characterID])) $row["characters"]["$characterID"] = 0;
-				$lastKillID = $row["characters"]["$characterID"];
+				$errorCode = (int) $ex->getCode();
+				if ($errorCode == 904) { Util::out("(apiProducer) 904'ed"); exit(); }
+				if ($errorCode == 28)
+				{
+					Util::out("(apiProducer) API Server timeout");
+					exit();
+				}
+				$tqApis->remove($row); // Problem with api the key, remove it from rotation
+				if ($errorCode != 221 && $debug) Util::out("(apiProducer) Error Validating $keyID: " . $ex->getCode() . " " . $ex->getMessage());
+				$apis->update(['keyID' => $keyID, 'vCode' => $vCode], ['$set' => ['errorCode' => $errorCode]]);
+				continue;
+			}    
 
-				// Make sure we have the names and id's in the information table
-				$mdb->insertUpdate("information", ['type' => 'corporationID', 'id' => ((int) $character->corporationID)], ['name' => ((string) $character->corporationName)]);
-				$mdb->insertUpdate("information", ['type' => 'characterID', 'id' => ((int) $characterID)], ['name' => ((string) $character->characterName), 'corporationID' => ((int) $character->corporationID)]);
+			$key = @$apiKeyInfo->key;
+			$accessMask = @$key->accessMask;
+			$characterIDs = array();
+			if ($accessMask & 256)
+			{
+				foreach ($apiKeyInfo->key->characters as $character)
+				{
+					$characterID = (int) $character->characterID;
+					if (!isset($row["characters"][$characterID])) $row["characters"]["$characterID"] = 0;
+					$lastKillID = $row["characters"]["$characterID"];
 
-				$type = $apiKeyInfo->key->type;
-				if ($debug) Util::out("Adding $keyID $characterID $type $vCode");
+					// Make sure we have the names and id's in the information table
+					$mdb->insertUpdate("information", ['type' => 'corporationID', 'id' => ((int) $character->corporationID)], ['name' => ((string) $character->corporationName)]);
+					$mdb->insertUpdate("information", ['type' => 'characterID', 'id' => ((int) $characterID)], ['name' => ((string) $character->characterName), 'corporationID' => ((int) $character->corporationID)]);
 
-				$char = ['keyID' => $keyID, 'vCode' => $vCode, 'characterID' => $characterID, 'type' => $type];
-				$tqApiChars->add($char);
+					$type = $apiKeyInfo->key->type;
+					if ($debug) Util::out("Adding $keyID $characterID $type $vCode");
+
+					$char = ['keyID' => $keyID, 'vCode' => $vCode, 'characterID' => $characterID, 'type' => $type];
+					$tqApiChars->add($char);
+				}
 			}
 		}
 	}
--- cron/1.apiConsumer.php
+++ cron/1.apiConsumer.php
@@ -2,9 +2,9 @@
 
 for ($i = 0; $i < 20; $i++)
 {
-        $pid = pcntl_fork();
-        if ($pid == -1) exit();
-        if ($pid == 0) break;
+	$pid = pcntl_fork();
+	if ($pid == -1) exit();
+	if ($pid == 0) break;
 }
 if ($pid != 0) exit();
 
@@ -17,100 +17,103 @@ $count = 0;
 while ($timer->stop() <= 59000)
 {
 	$row = $tqApiChars->next();
-	if ($row === null) continue;
-
-	$charID = $row["characterID"];
-	$keyID = $row["keyID"];
-	$vCode = $row["vCode"];
-	$type = $row["type"];
-	$charCorp = $type == "Corporation" ? "corp" : "char";
-	$killsAdded = 0;
-
-	\Pheal\Core\Config::getInstance()->http_method = "curl";
-	\Pheal\Core\Config::getInstance()->http_user_agent = "API Fetcher for http://$baseAddr";
-	\Pheal\Core\Config::getInstance()->http_post = false;
-	\Pheal\Core\Config::getInstance()->http_keepalive = 30; // KeepAliveTimeout in seconds
-	\Pheal\Core\Config::getInstance()->http_timeout = 60;
-	\Pheal\Core\Config::getInstance()->api_customkeys = true;
-	\Pheal\Core\Config::getInstance()->api_base = "https://api.eveonline.com/";
-	$pheal = new \Pheal\Pheal($keyID, $vCode);
-
-	$charCorp = ($type == "Corporation" ? 'corp' : 'char');
-	$pheal->scope = $charCorp;
-	$result = null;
-
-	$params = array();
-	$params['characterID'] = $charID;
-	$result = null;
-
-	try 
+	if ($row !== null)
 	{
-		$result = $pheal->KillMails($params);
-	} catch (Exception $ex)
-	{
-		$errorCode = $ex->getCode();
-		if ($errorCode == 904) { Util::out("(apiConsumer) 904'ed..."); exit(); }
-		if ($errorCode == 28)
+		$charID = $row["characterID"];
+		$keyID = $row["keyID"];
+		$vCode = $row["vCode"];
+		$type = $row["type"];
+		$charCorp = $type == "Corporation" ? "corp" : "char";
+		$killsAdded = 0;
+
+		\Pheal\Core\Config::getInstance()->http_method = "curl";
+		\Pheal\Core\Config::getInstance()->http_user_agent = "API Fetcher for http://$baseAddr";
+		\Pheal\Core\Config::getInstance()->http_post = false;
+		\Pheal\Core\Config::getInstance()->http_keepalive = 30; // KeepAliveTimeout in seconds
+		\Pheal\Core\Config::getInstance()->http_timeout = 60;
+		\Pheal\Core\Config::getInstance()->api_customkeys = true;
+		\Pheal\Core\Config::getInstance()->api_base = "https://api.eveonline.com/";
+		$pheal = new \Pheal\Pheal($keyID, $vCode);
+
+		$charCorp = ($type == "Corporation" ? 'corp' : 'char');
+		$pheal->scope = $charCorp;
+		$result = null;
+
+		$params = array();
+		$params['characterID'] = $charID;
+		$result = null;
+
+		try 
 		{
-			Util::out("(apiConsumer) API Server timeout");
-			exit();
-		}
-		// Error code 0: Scotty is up to his shenanigans again (aka server issue)
-		// Error code 221: server randomly throwing an illegal access error even though this is a legit call
-		if ($errorCode != 0 && $errorCode != 221) $tqApiChars->remove($row);
-		continue;
-	}
-	$newMaxKillID = 0;
-	foreach ($result->kills as $kill)
-	{
-		$killID = (int) $kill->killID;
-
-		$newMaxKillID = (int) max($newMaxKillID, $killID);
-
-		$json = json_encode($kill->toArray());
-		$killmail = json_decode($json, true);
-		$killmail["killID"] = (int) $killID; // make sure killID is an int;
-		if (!$mdb->exists("crestmails", ['killID' => $killID]) && !$mdb->exists("apimails", ['killID' => $killID])) $mdb->insertUpdate("apimails", $killmail);
-
-		$victim = $killmail["victim"];
-		$victimID = $victim["characterID"] == 0 ? "None" : $victim["characterID"];
-
-		$attackers = $killmail["attackers"];
-		$attacker = null;
-		if ($attackers != null) foreach($attackers as $att)
+			$result = $pheal->KillMails($params);
+		} catch (Exception $ex)
 		{
-			if ($att["finalBlow"] != 0) $attacker = $att;
+			$errorCode = $ex->getCode();
+			if ($errorCode == 904) { Util::out("(apiConsumer) 904'ed..."); exit(); }
+			if ($errorCode == 28)
+			{
+				Util::out("(apiConsumer) API Server timeout");
+				exit();
+			}
+			// Error code 0: Scotty is up to his shenanigans again (aka server issue)
+			// Error code 221: server randomly throwing an illegal access error even though this is a legit call
+			if ($errorCode != 0 && $errorCode != 221) $tqApiChars->remove($row);
+			continue;
 		}
-		if ($attacker == null) $attacker = $attackers[0];
-		$attackerID = $attacker["characterID"] == 0 ? "None" : $attacker["characterID"];
+		$newMaxKillID = 0;
+		foreach ($result->kills as $kill)
+		{
+			$killID = (int) $kill->killID;
 
-		$shipTypeID = $victim["shipTypeID"];
+			$newMaxKillID = (int) max($newMaxKillID, $killID);
 
-		$dttm = (strtotime($killmail["killTime"]) * 10000000) + 116444736000000000;
+			$json = json_encode($kill->toArray());
+			$killmail = json_decode($json, true);
+			$killmail["killID"] = (int) $killID; // make sure killID is an int;
+			if (!$mdb->exists("crestmails", ['killID' => $killID]) && !$mdb->exists("apimails", ['killID' => $killID])) $mdb->insertUpdate("apimails", $killmail);
 
-		$string = "$victimID$attackerID$shipTypeID$dttm";
+			$victim = $killmail["victim"];
+			$victimID = $victim["characterID"] == 0 ? "None" : $victim["characterID"];
 
-		$hash = sha1($string);
+			$attackers = $killmail["attackers"];
+			$attacker = null;
+			if ($attackers != null) foreach($attackers as $att)
+			{
+				if ($att["finalBlow"] != 0) $attacker = $att;
+			}
+			if ($attacker == null) $attacker = $attackers[0];
+			$attackerID = $attacker["characterID"] == 0 ? "None" : $attacker["characterID"];
 
-		$killInsert = ['killID' => (int) $killID, 'hash' => $hash];
-		$exists = $mdb->exists("crestmails", $killInsert);
-		if (!$exists) $mdb->getCollection("crestmails")->save(['killID' => (int) $killID, 'hash' => $hash, 'processed' => false, 'source' => 'api', 'added' => $mdb->now()]);
-		if (!$exists) $killsAdded++;
-		if (!$exists && $debug) Util::out("Added $killID from API");
-	}
+			$shipTypeID = $victim["shipTypeID"];
 
-	// helpful info for output if needed
-	$info = $mdb->findDoc("information", ['type' => 'characterID', 'id' => $charID], [], [ 'name' => 1, 'corporationID' => 1]);
-	$corpInfo = $mdb->findDoc("information", ['type' => 'corporationID', 'id' => @$info["corporationID"]], [], [ 'name' => 1]);
-	$redis->setex("apiVerified:" . ($type == "Corporation" ? @$corpInfo["id"] : $charID), 9600, true);
+			$dttm = (strtotime($killmail["killTime"]) * 10000000) + 116444736000000000;
 
-	// If we got new kills tell the log about it
-	if ($killsAdded > 0)
-	{
-		if ($type == "Corporation") $name = "corp " . @$corpInfo["name"];
-		else $name = "char " . @$info["name"];
-		while (strlen("$killsAdded") < 3) $killsAdded = " " . $killsAdded;
-		Util::out("$killsAdded kills added by $name");
+			$string = "$victimID$attackerID$shipTypeID$dttm";
+
+			$hash = sha1($string);
+
+			$killInsert = ['killID' => (int) $killID, 'hash' => $hash];
+			$exists = $mdb->exists("crestmails", $killInsert);
+			if (!$exists) $mdb->getCollection("crestmails")->save(['killID' => (int) $killID, 'hash' => $hash, 'processed' => false, 'source' => 'api', 'added' => $mdb->now()]);
+			if (!$exists) $killsAdded++;
+			if (!$exists && $debug) Util::out("Added $killID from API");
+		}
+
+		// helpful info for output if needed
+		$info = $mdb->findDoc("information", ['type' => 'characterID', 'id' => $charID], [], [ 'name' => 1, 'corporationID' => 1]);
+		$corpInfo = $mdb->findDoc("information", ['type' => 'corporationID', 'id' => @$info["corporationID"]], [], [ 'name' => 1]);
+
+		$redis->setex("apiVerified:" . ($type == "Corporation" ? @$info["corporationID"] : $charID), 86400, true);
+		if ($newMaxKillID == 0) $tqApiChars->setTime($row, time() + rand(72000, 86400));
+
+		// If we got new kills tell the log about it
+		if ($killsAdded > 0)
+		{
+			if ($type == "Corporation") $name = "corp " . @$corpInfo["name"];
+			else $name = "char " . @$info["name"];
+			while (strlen("$killsAdded") < 3) $killsAdded = " " . $killsAdded;
+			Util::out("$killsAdded kills added by $name");
+		}
 	}
 	sleep(1);
 }
