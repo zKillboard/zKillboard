d1bd6e37850530108f659edc193cd3f1ef5ccf93
Clinton Weiss <cvweiss@gmail.com>
2015-07-15 21:28:34 +0000

    display TQ status to end user

---
+++ cron/tqStatus.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "../init.php";
+
+$root = CrestTools::getJSON("https://public-crest.eveonline.com/");
+
+$serverStatus = @$root["serviceStatus"]["eve"];
+if ($serverStatus == null) $serverStatus = "OFFLINE";
+else $serverStatus = strtoupper($serverStatus);
+$loggedIn = (int) @$root["userCounts"]["eve"];
+
+if ($loggedIn == 0) $loggedIn = $serverStatus;
+else $loggedIn = number_format($loggedIn, 0);
+
+$redis->set("tqStatus", $serverStatus);
+$redis->set("tqCount", $loggedIn);
