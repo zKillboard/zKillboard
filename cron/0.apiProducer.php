68cdab96e8a8b44dc423e2ba31ee0fbf1a23d508
Clinton Weiss <cvweiss@gmail.com>
2015-09-02 17:14:01 +0000

    slightly different approach on api error handling, needs more work under redis

--- cron/0.apiProducer.php
+++ cron/0.apiProducer.php
@@ -18,10 +18,6 @@ $tqApis = new RedisTimeQueue('tqApis', 9600);
 $tqApiChars = new RedisTimeQueue('tqApiChars');
 
 if ($pid != 0 && date('i') % 5 == 0) {
-    $apis->update(['killID' => null], ['$set' => ['killID' => 0]], ['multiple' => true]);
-    $apis->update(['errorCode' => null], ['$set' => ['errorCode' => 0]], ['multiple' => true]);
-    $apis->update(['errorCode' => 28], ['$set' => ['errorCode' => 0]], ['multiple' => true]);
-
     $allApis = $mdb->find('apis');
     foreach ($allApis as $api) {
         $keyID = $api['keyID'];
@@ -29,14 +25,11 @@ if ($pid != 0 && date('i') % 5 == 0) {
         $userID = $api['userID'];
         $value = ['keyID' => $keyID, 'vCode' => $vCode, 'userID' => $userID];
         $errorCode = (int) @$api['errorCode'];
-        if (in_array($errorCode, [0, 18, 106, 503, 522, 901, 902, 904])) {
-            $tqApis->add($value);
-        }
+        if (in_array($errorCode, [106, 203, 220, 222, 404, 522])) continue;
+        $tqApis->add($value);
     }
 }
-if ($pid != 0) {
-    exit();
-}
+if ($pid != 0) exit();
 
 $timer = new Timer();
 $requestNum = 0;
@@ -52,62 +45,62 @@ while ($timer->stop() <= 58000) {
             $row['characters'] = [];
         }
 
-        $errorCode = (int) @$row['errorCode'];
-        if ($errorCode == 0 || $errorCode == 221) {
-            \Pheal\Core\Config::getInstance()->http_user_agent = "API Fetcher for https://$baseAddr";
-            \Pheal\Core\Config::getInstance()->http_post = false;
-            \Pheal\Core\Config::getInstance()->http_keepalive = true; // default 15 seconds
-            \Pheal\Core\Config::getInstance()->http_keepalive = 10; // KeepAliveTimeout in seconds
-            \Pheal\Core\Config::getInstance()->http_timeout = 30;
-            \Pheal\Core\Config::getInstance()->api_customkeys = true;
+        $errorCode = (int) $mdb->findField("apis", "errorCode", ['keyID' => $keyID, 'vCode' => $vCode]);
+	if (in_array($errorCode, [106, 203, 220, 222, 404, 522])) continue;
+	\Pheal\Core\Config::getInstance()->http_user_agent = "API Fetcher for https://$baseAddr";
+	\Pheal\Core\Config::getInstance()->http_post = false;
+	\Pheal\Core\Config::getInstance()->http_keepalive = true; // default 15 seconds
+	\Pheal\Core\Config::getInstance()->http_keepalive = 10; // KeepAliveTimeout in seconds
+	\Pheal\Core\Config::getInstance()->http_timeout = 30;
+	\Pheal\Core\Config::getInstance()->api_customkeys = true;
 
-            $pheal = new \Pheal\Pheal($keyID, $vCode);
-            try {
-                $apiKeyInfo = $pheal->ApiKeyInfo();
-            } catch (Exception $ex) {
-                $tqApis->remove($row); // Problem with api the key, remove it from rotation
-                $errorCode = (int) $ex->getCode();
-                if ($errorCode == 904) {
-                    Util::out("(apiProducer) 904'ed");
-                    exit();
-                }
-                if ($errorCode == 28) {
-                    Util::out('(apiProducer) API Server timeout');
-                    exit();
-                }
-                if ($errorCode != 221 && $debug) {
-                    Util::out("(apiProducer) Error Validating $keyID: ".$ex->getCode().' '.$ex->getMessage());
-                }
-                $apis->update(['keyID' => $keyID, 'vCode' => $vCode], ['$set' => ['errorCode' => $errorCode]]);
-                sleep(3);
-                continue;
-            }
+	$pheal = new \Pheal\Pheal($keyID, $vCode);
+	try {
+		$apiKeyInfo = $pheal->ApiKeyInfo();
+		$apis->update(['keyID' => $keyID, 'vCode' => $vCode], ['$set' => ['errorCode' => 0]]);
+	} catch (Exception $ex) {
+		$tqApis->remove($row); // Problem with api the key, remove it from rotation
+		$errorCode = (int) $ex->getCode();
+		if ($errorCode == 904) {
+			Util::out("(apiProducer) 904'ed");
+			exit();
+		}
+		if ($errorCode == 28) {
+			Util::out('(apiProducer) API Server timeout');
+			exit();
+		}
+		if ($errorCode != 221 && $debug) {
+			Util::out("(apiProducer) Error Validating $keyID: ".$ex->getCode().' '.$ex->getMessage());
+		}
+		$apis->update(['keyID' => $keyID, 'vCode' => $vCode], ['$set' => ['errorCode' => $errorCode]]);
+		sleep(3);
+		continue;
+	}
 
-            $key = @$apiKeyInfo->key;
-            $accessMask = @$key->accessMask;
-            $characterIDs = array();
-            if ($accessMask & 256) {
-                foreach ($apiKeyInfo->key->characters as $character) {
-                    $characterID = (int) $character->characterID;
-                    if (!isset($row['characters'][$characterID])) {
-                        $row['characters']["$characterID"] = 0;
-                    }
-                    $lastKillID = $row['characters']["$characterID"];
+	$key = @$apiKeyInfo->key;
+	$accessMask = @$key->accessMask;
+	$characterIDs = array();
+	if ($accessMask & 256) {
+		foreach ($apiKeyInfo->key->characters as $character) {
+			$characterID = (int) $character->characterID;
+			if (!isset($row['characters'][$characterID])) {
+				$row['characters']["$characterID"] = 0;
+			}
+			$lastKillID = $row['characters']["$characterID"];
 
-                    // Make sure we have the names and id's in the information table
-                    $mdb->insertUpdate('information', ['type' => 'corporationID', 'id' => ((int) $character->corporationID)], ['name' => ((string) $character->corporationName)]);
-                    $mdb->insertUpdate('information', ['type' => 'characterID', 'id' => ((int) $characterID)], ['name' => ((string) $character->characterName), 'corporationID' => ((int) $character->corporationID)]);
+			// Make sure we have the names and id's in the information table
+			$mdb->insertUpdate('information', ['type' => 'corporationID', 'id' => ((int) $character->corporationID)], ['name' => ((string) $character->corporationName)]);
+			$mdb->insertUpdate('information', ['type' => 'characterID', 'id' => ((int) $characterID)], ['name' => ((string) $character->characterName), 'corporationID' => ((int) $character->corporationID)]);
 
-                    $type = $apiKeyInfo->key->type;
-                    if ($debug) {
-                        Util::out("Adding $keyID $characterID $type $vCode");
-                    }
+			$type = $apiKeyInfo->key->type;
+			if ($debug) {
+				Util::out("Adding $keyID $characterID $type $vCode");
+			}
 
-                    $char = ['keyID' => $keyID, 'vCode' => $vCode, 'characterID' => $characterID, 'type' => $type, 'userID' => $userID];
-                    $tqApiChars->add($char);
-                }
-            }
-        }
+			$char = ['keyID' => $keyID, 'vCode' => $vCode, 'characterID' => $characterID, 'type' => $type, 'userID' => $userID];
+			$tqApiChars->add($char);
+		}
+	}
     }
     sleep(1);
 }
