257a1e41bb9b4e3916a32f0a9326a392bd96bda6
Clinton Weiss <cvweiss@gmail.com>
2015-10-07 21:16:01 +0000

    durr, can't rely on atomic redis actions with php code, first draft for now

--- classes/RedisTimeQueue.php
+++ classes/RedisTimeQueue.php
@@ -41,15 +41,23 @@ class RedisTimeQueue
     {
         global $redis;
 
+        $sem = sem_get(6398);
+        if (!sem_acquire($sem)) {
+            throw new Exception('Unable to obtain kmdb semaphore');
+        }
+
         $next = $redis->zRange($this->queueName, 0, 0, true);
         if ($next === null) {
+	    sem_release($sem);
             return;
         }
         if (!is_array($next)) {
+	    sem_release($sem);
             return;
         }
         $value = key($next);
         if (!isset($next[$value])) {
+	    sem_release($sem);
             return;
         }
         $time = $next[$value];
@@ -58,11 +66,13 @@ class RedisTimeQueue
             return;
         }
         if ($time >= time()) {
+	    sem_release($sem);
             sleep(1); // Block...
             return $this->next(false);
         }
 
         $redis->zAdd($this->queueName, time() + $this->deltaSeconds, $value);
+	sem_release($sem);
         $value = unserialize($value);
 
         return $value;
