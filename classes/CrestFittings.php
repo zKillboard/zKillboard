d18311710af9d8884d8f316c39a7454542208411
Clinton Weiss <cvweiss@gmail.com>
2015-10-22 19:45:03 +0000

    now successfully exporting fittings to eve client

--- classes/CrestFittings.php
+++ classes/CrestFittings.php
@@ -15,7 +15,52 @@ class CrestFittings {
 	}
 
 	public static function saveFitting($killID) {
+		global $mdb, $redis;
+	
+		$killmail = $mdb->findDoc('rawmails', ['killID' => (int) $killID]);
+		$victim = $killmail['victim'];
+		header('Content-Type: application/json');
+		$export = [];
+		$export['name'] = $victim['character']['name'] . "'s " . $victim['shipType']['name'];
+		$export['description'] = "Imported from zKillboard";
+		$export['ship'] = ['id' => $victim['shipType']['id']];
+		$export['ship']['name'] = $victim['shipType']['name'];
+		$export['ship']['href'] = $victim['shipType']['href'];
 
+		$items = $victim['items'];
+		foreach ($items as $item) {
+			$flag = $item['flag'];
+			if (!self::isFittableFlag($flag)) continue;
+			$nextItem = [];	
+			$nextItem['flag'] = $flag;
+			$nextItem['quantity'] = @$item['quantityDropped'] + @$item['quantityDestroyed'];
+			$nextItem['type']['id'] = $item['itemType']['id'];
+			$nextItem['type']['name'] = $item['itemType']['name'];
+			$nextItem['type']['href'] = $item['itemType']['href'];
+			$export['items'][] = $nextItem;
+		}
 
+		$r = CrestSSO::crestGet("https://api-sisi.testeveonline.com/decode/");
+		$character = CrestSSO::crestGet($r['character']['href']);
+		$result = CrestSSO::crestPost($character['fittings']['href'], $export);
+		if ($result['httpCode'] == 201) return ['message' => 'Fit successfully saved to Eve client.'];
+		return $result;
+	}
+
+	private static $infernoFlags = array(
+			4 => array(116, 121),
+			12 => array(27, 34), // Highs
+			13 => array(19, 26), // Mids
+			11 => array(11, 18), // Lows
+			87 => array(87, 87), // Drones
+			2663 => array(92, 98), // Rigs
+			3772 => array(125, 132), // Subs
+			);
+
+	public static function isFittableFlag($flag) {
+		foreach (self::$infernoFlags as $key=>$range) {
+			if ($flag >= $range[0] && $flag <= $range[1]) return true;
+		}
+		return false;
 	}
 }
