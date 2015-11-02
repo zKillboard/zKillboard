cd3b217980ee1aed61b67f64b9fe551e6cdae189
Clinton Weiss <cvweiss@gmail.com>
2015-11-02 22:10:09 +0000

    report unknown if unable to connect to tq's crest

--- cron/tqStatus.php
+++ cron/tqStatus.php
@@ -4,18 +4,22 @@ require_once '../init.php';
 
 $root = CrestTools::getJSON('https://public-crest.eveonline.com/');
 
-$serverStatus = @$root['serviceStatus']['eve'];
-if ($serverStatus == null) {
-    $serverStatus = 'OFFLINE';
+if ($root == 0) {
+	$serverStatus = 'UNKNOWN';
 } else {
-    $serverStatus = strtoupper($serverStatus);
+	$serverStatus = @$root['serviceStatus']['eve'];
+	if ($serverStatus == null) {
+		$serverStatus = 'OFFLINE';
+	} else {
+		$serverStatus = strtoupper($serverStatus);
+	}
 }
 $loggedIn = (int) @$root['userCounts']['eve'];
 
 if ($loggedIn == 0) {
-    $loggedIn = $serverStatus;
+	$loggedIn = $serverStatus;
 } else {
-    $loggedIn = number_format($loggedIn, 0);
+	$loggedIn = number_format($loggedIn, 0);
 }
 
 $redis->set('tqStatus', $serverStatus);
