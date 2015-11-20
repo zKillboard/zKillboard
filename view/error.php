3b34323b4333e75d314d8e1e2c136da86a4acbb9
Clinton Weiss <cvweiss@gmail.com>
2015-11-20 20:03:59 +0000

    removed zz_errors dependency

--- view/error.php
+++ view/error.php
@@ -9,20 +9,20 @@ $codeHash = md5($trace);
 
 $html = '';
 if ($code) {
-    $html .= sprintf('<div><strong>Code:</strong> %s</div>', $code);
+	$html .= sprintf('<div><strong>Code:</strong> %s</div>', $code);
 }
 if ($message) {
-    $html .= sprintf('<div><strong>Message:</strong> %s</div>', $message);
+	$html .= sprintf('<div><strong>Message:</strong> %s</div>', $message);
 }
 if ($file) {
-    $html .= sprintf('<div><strong>File:</strong> %s</div>', $file);
+	$html .= sprintf('<div><strong>File:</strong> %s</div>', $file);
 }
 if ($line) {
-    $html .= sprintf('<div><strong>Line:</strong> %s</div>', $line);
+	$html .= sprintf('<div><strong>Line:</strong> %s</div>', $line);
 }
 if ($trace) {
-    $html .= '<h4>Trace</h4>';
-    $html .= sprintf('<pre>%s</pre>', $trace);
+	$html .= '<h4>Trace</h4>';
+	$html .= sprintf('<pre>%s</pre>', $trace);
 }
 
 $date = date('Y-m-d H:i:s');
@@ -30,16 +30,16 @@ $url = $_SERVER['REQUEST_URI'];
 $ip = IP::get();
 
 try {
-    Db::execute('INSERT INTO zz_errors (id, error, message, url, ip) VALUES (:id, :error, :message, :url, :ip) ON DUPLICATE KEY UPDATE ip = :ip, date = :date', array(':id' => $codeHash, ':error' => $html, ':message' => $message, ':url' => $url, ':ip' => $ip, ':date' => $date));
-    $app->render('error.html', array('code' => $codeHash, 'message' => $message, 'error' => $html));
+	$mdb->save("errors", ['code' => $code, 'message' => $message, 'file' => $file, 'line' => $line, 'stack' => $trace, 'hash' => $codeHash, 'ip' => $ip, 'date' => $date, 'url' => $url]);
+	$app->render('error.html', array('code' => $codeHash, 'message' => $message, 'error' => $html));
 } catch (Exception $ex) {
-    $html = '<html>';
-    $html .= '<head>';
-    $html .= '<title>Oh noes an error!</title>';
-    $html .= '</head>';
-    $html .= '<body>';
-    $html .= $e->getMessage();
-    $html .= '</body>';
-    $html .= '</html>';
-    echo $html;
+	$html = '<html>';
+	$html .= '<head>';
+	$html .= '<title>Oh noes an error!</title>';
+	$html .= '</head>';
+	$html .= '<body>';
+	$html .= $e->getMessage();
+	$html .= '</body>';
+	$html .= '</html>';
+	echo $html;
 }
