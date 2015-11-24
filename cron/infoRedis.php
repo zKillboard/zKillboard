0a2a45d93b70f709b9b92b7e7b16a2cd507ae5f1
Clinton Weiss <cvweiss@gmail.com>
2015-11-24 17:44:38 +0000

    make sure entries expire

--- cron/infoRedis.php
+++ cron/infoRedis.php
@@ -2,27 +2,25 @@
 
 require_once '../init.php';
 
-$i = date('i');
-if ($i != 15) {
-    exit();
-}
+if ($date('i') != 15) exit();
 
 $information = $mdb->getCollection('information');
 $types = $mdb->getCollection('information')->distinct('type');
 
 foreach ($types as $type) {
-    if ($type == 'warID') {
-        continue;
-    }
-    $typeRows = $information->find(['type' => $type]);
-    Util::out("Adding $type to redis");
-    foreach ($typeRows as $row) {
-        unset($row['_id']);
-        $id = $row['id'];
-        $key = "tq:$type:$id";
-        $multi = $redis->multi();
-        $multi->del($key);
-        $multi->hMSet($key, $row);
-        $multi->exec();
-    }
+	if ($type == 'warID') {
+		continue;
+	}
+	$typeRows = $information->find(['type' => $type]);
+	Util::out("Adding $type to redis");
+	foreach ($typeRows as $row) {
+		unset($row['_id']);
+		$id = $row['id'];
+		$key = "tq:$type:$id";
+		$multi = $redis->multi();
+		$multi->del($key);
+		$multi->hMSet($key, $row);
+		$multi->expire($key, 86400);
+		$multi->exec();
+	}
 }
