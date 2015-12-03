b9a1924720b8ae15bfb34c6705bb840f9b7f4aa4
Clinton Weiss <cvweiss@gmail.com>
2015-12-03 14:19:10 +0000

    cleaned up error handling code

--- cron/2.crest_fetch.php
+++ cron/2.crest_fetch.php
@@ -13,99 +13,103 @@ $killsLastHour = new RedisTtlCounter('killsLastHour');
 $counter = 0;
 $timer = new Timer();
 while (!Util::exitNow() && $timer->stop() < 115000) {
-    $unprocessed = $crestmails->find(array('processed' => false))->sort(['killID' => -1])->limit(10);
+	$unprocessed = $crestmails->find(array('processed' => false))->sort(['killID' => -1])->limit(10);
 
-    if (!$unprocessed->hasNext()) {
-        sleep(1);
-    }
-    foreach ($unprocessed as $crestmail) {
-        if (Util::exitNow()) {
-            break;
-        }
-        $id = $crestmail['killID'];
-        $hash = $crestmail['hash'];
+	if (!$unprocessed->hasNext()) {
+		sleep(1);
+	}
+	foreach ($unprocessed as $crestmail) {
+		if (Util::exitNow()) {
+			break;
+		}
+		$id = $crestmail['killID'];
+		$hash = $crestmail['hash'];
 
-        if ($mdb->exists('killmails', ['killID' => $id])) {
-            $crestmails->update($crestmail, array('$set' => array('processed' => true)));
-            continue;
-        }
+		if ($mdb->exists('killmails', ['killID' => $id])) {
+			$crestmails->update($crestmail, array('$set' => array('processed' => true)));
+			continue;
+		}
 
-        $killmail = CrestTools::fetch($id, $hash);
-        if ($killmail == null || $killmail == '' || !isset($killmail['attackers'])) {
-            if ($killmail == 403) {
-                $mdb->remove('crestmails', $crestmail);
-            } else {
-                $crestmails->update($crestmail, array('$set' => array('processed' => null)));
-            }
-            continue;
-        }
-        if ($killmail == 415 || $killmail == 500 || $killmail == 403) {
-            $crestmails->update($crestmail, array('$set' => array('processed' => true, 'errorCode' => $killmail)));
-            continue;
-        }
-        unset($crestmail['npcOnly']);
-        unset($killmail['zkb']);
-        unset($killmail['_id']);
+		$killmail = CrestTools::fetch($id, $hash);
+		if (!isset($killmail['attackers'])) $killmail = null;
+		switch ($killmail) {
+			case 403:
+				$mdb->remove('crestmails', $crestmail);
+				continue;
+			case 503:
+				$crestmails->update($crestmail, array('$set' => array('processed' => false, 'errorCode' => $killmail)));
+				continue;
+			case 415:
+			case 500:
+			case '':
+			case null:
+				$crestmails->update($crestmail, array('$set' => array('processed' => null, 'error' => $error)));
+				continue;
+		}
 
-        if (!$mdb->exists('rawmails', ['killID' => (int) $id])) {
-            $killsLastHour->add($id);
-            $rawmails->save($killmail);
-        }
+		unset($crestmail['npcOnly']);
+		unset($killmail['zkb']);
+		unset($killmail['_id']);
 
-        if (!validKill($killmail)) {
-            $crestmail['npcOnly'] = true;
-            $crestmail['processed'] = true;
-            $crestmails->save($crestmail);
-            continue;
-        }
+		if (!$mdb->exists('rawmails', ['killID' => (int) $id])) {
+			$killsLastHour->add($id);
+			$rawmails->save($killmail);
+		}
 
-        $killID = @$killmail['killID'];
-        if ($killID != 0) {
-            $crestmail['processed'] = true;
-            $crestmails->save($crestmail);
-            $queueProcess->push($killID);
-            ++$counter;
+		if (!validKill($killmail)) {
+			$crestmail['npcOnly'] = true;
+			$crestmail['processed'] = true;
+			$crestmails->save($crestmail);
+			continue;
+		}
 
-            $queueShare->push($killID);
-        } else {
-            $crestmails->update($crestmail, array('$set' => array('processed' => null)));
-        }
-    }
+		$killID = @$killmail['killID'];
+		if ($killID != 0) {
+			$crestmail['processed'] = true;
+			$crestmails->save($crestmail);
+			$queueProcess->push($killID);
+			++$counter;
+
+			$queueShare->push($killID);
+		} else {
+			$crestmails->update($crestmail, array('$set' => array('processed' => false)));
+		}
+	}
 }
 if ($debug && $counter > 0) {
-    Util::out('Added '.number_format($counter, 0).' Kills.');
+	Util::out('Added '.number_format($counter, 0).' Kills.');
 }
 
 function validKill(&$kill)
 {
-    // Show all pod kills
-    $victimShipID = $kill['victim']['shipType']['id'];
-    if ($victimShipID == 670 || $victimShipID == 33328) {
-        return true;
-    }
+	// Show all pod kills
+	$victimShipID = $kill['victim']['shipType']['id'];
+	if ($victimShipID == 670 || $victimShipID == 33328) {
+		return true;
+	}
 
-    $npcOnly = true;
-    foreach ($kill['attackers'] as $attacker) {
-        if (isset($attacker['shipType']['id'])) {
-            $attackerGroupID = Info::getGroupID($attacker['shipType']['id']);
-            if ($attackerGroupID == 365) {
-                return true;
-            } // A tower is involved
-            if ($attackerGroupID == 99) {
-                return true;
-            } // A sentry gun is involved
-        }
+	$npcOnly = true;
+	foreach ($kill['attackers'] as $attacker) {
+		if (isset($attacker['shipType']['id'])) {
+			$attackerGroupID = Info::getGroupID($attacker['shipType']['id']);
+			if ($attackerGroupID == 365) {
+				return true;
+			} // A tower is involved
+			if ($attackerGroupID == 99) {
+				return true;
+			} // A sentry gun is involved
+		}
 
-        if (isset($attacker['shipType']['id']) && $attacker['shipType']['id'] == 34495) {
-            return true;
-        } // A drifter is involved
+		if (isset($attacker['shipType']['id']) && $attacker['shipType']['id'] == 34495) {
+			return true;
+		} // A drifter is involved
 
-        // Don't process the kill if it's NPC only
-        if (isset($attacker['corporation']['id']) && $attacker['corporation']['id'] == 1000125) {
-            return true;
-        }
-        $npcOnly &= @$attacker['character']['id'] == 0 && (@$attacker['corporation']['id'] < 1999999 && @$attacker['corporation']['id'] != 1000125);
-    }
+		// Don't process the kill if it's NPC only
+		if (isset($attacker['corporation']['id']) && $attacker['corporation']['id'] == 1000125) {
+			return true;
+		}
+		$npcOnly &= @$attacker['character']['id'] == 0 && (@$attacker['corporation']['id'] < 1999999 && @$attacker['corporation']['id'] != 1000125);
+	}
 
-    return !$npcOnly;
+	return !$npcOnly;
 }
