efd39f2553e87425a44ea3384904f5e95d0feddb
Clinton Weiss <cvweiss@gmail.com>
2015-12-03 21:32:18 +0000

    now uses redis for all time ranks, first step in ranks updating with each killmail

--- cron/9.allTimeRanks.php
+++ cron/9.allTimeRanks.php
@@ -1,93 +1,92 @@
 <?php
 
-require_once '../init.php';
+require_once "../init.php";
 
-$now = date('Hi');
-if ($now != '1441') {
-    exit();
+$today = date('Ymd');
+$todaysKey = "RC:alltimeRanksCalculated:$today";
+if ($redis->get($todaysKey) == true) exit();
+
+$statClasses = ['ships', 'isk', 'points'];
+$statTypes = ['Destroyed', 'Lost'];
+$cleanup = [];
+$expires = [];
+
+$information = $mdb->getCollection("statistics");
+
+Util::out("All time ranks - first iteration");
+$iter = $information->find();
+while ($row = $iter->next()) {
+	$type = $row['type'];
+	$id = $row['id'];
+	$key = "tq:ranks:$type";
+	foreach ($statClasses as $statClass) {
+		foreach ($statTypes as $statType) {
+			$value = (int) @$row["${statClass}${statType}"];
+			$redisKey = "${key}:${statClass}${statType}";
+			$redis->zAdd($redisKey, $value, $id);
+			$cleanup[$redisKey] = true;
+		}
+	}
 }
 
-$date = new MongoDate(strtotime(date('Y-m-d')));
-$types = ['corporationID', 'allianceID', 'factionID', 'shipTypeID', 'groupID', 'solarSystemID', 'regionID', 'characterID'];
-$categories = ['ships', 'isk', 'points'];
+Util::out("All time ranks - second iteration");
+$iter = $information->find();
+while ($row = $iter->next()) {
+	$type = $row['type'];
+	$id = $row['id'];
+	$key = "tq:ranks:$type";
+	$update = [];
+	foreach ($statClasses as $statClass) {
+		foreach ($statTypes as $statType) {
+			$rank = 1 + $redis->zRevRank("${key}:${statClass}${statType}", $id);
+			$update["${statClass}${statType}Rank"] = $rank;
+		}
+	}
 
-foreach ($types as $type) {
-    Util::out("Starting all time ranks for $type");
-    $size = $mdb->count('statistics', ['type' => $type]);
-    $rankingIDs = [];
+	$shipsDestroyed = getValue($row, 'shipsDestroyed');
+	$shipsDestroyedRank = getValue($update, 'shipsDestroyedRank');
+	$shipsLost = getValue($row, 'shipsLost');
+	$shipsLostRank = getValue($update, 'shipsLostRank');
+	$shipsEff = ($shipsDestroyed / ($shipsDestroyed + $shipsLost));
 
-    foreach ($categories as $category) {
-        for ($i = 0; $i <= 1; ++$i) {
-            $field = $category.($i == 0 ? 'Destroyed' : 'Lost');
-            Util::out("$type Overall ranking $field");
+	$iskDestroyed = getValue($row, 'iskDestroyed');
+	$iskDestroyedRank = getValue($update, 'iskDestroyedRank');
+	$iskLost = getValue($row, 'iskLost');
+	$iskLostRank = getValue($update, 'iskLostRank');
+	$iskEff = ($iskDestroyed / ($iskDestroyed + $iskLost));
 
-            $currentValue = -1;
-            $currentRank = 0;
+	$pointsDestroyed = getValue($row, 'pointsDestroyed');
+	$pointsDestroyedRank = getValue($row, 'pointsDestroyedRank');
+	$pointsLost = getValue($row, 'pointsLost');
+	$pointsLostRank = getValue($row, 'pointsLostRank');
+	$pointsEff = ($pointsDestroyed / ($pointsDestroyed + $pointsLost));
 
-            $mdb->getCollection('statistics')->update(['type' => $type, $field => null], ['$set' => [$field => 0]], ['multiple' => true, 'socketTimeoutMS' => -1]);
-            $allIDs = $mdb->find('statistics', ['type' => $type], [$field => -1], null, ['months' => 0, 'groups' => 0]);
-            $currentRank = 0;
-            foreach ($allIDs as $row) {
-                ++$currentRank;
-                $mdb->getCollection('statistics')->update($row, ['$set' => ["{$field}Rank" => $currentRank]]);
-            }
-        }
-    }
-
-    $size = $mdb->count('statistics', ['type' => $type]);
-    $counter = 0;
-    Util::out("$type Overall rank calcing");
-    $cursor = $mdb->find('statistics', ['type' => $type], [], null, ['months' => 0, 'groups' => 0]);
-    foreach ($cursor as $row) {
-        ++$counter;
-        $id = $row['id'];
-
-        $shipsDestroyed = getValue($row, 'shipsDestroyed', $size);
-        $shipsDestroyedRank = getValue($row, 'shipsDestroyedRank', $size);
-        $shipsLost = getValue($row, 'shipsLost', $size);
-        $shipsLostRank = getValue($row, 'shipsLostRank', $size);
-        $shipsEff = ($shipsDestroyed / ($shipsDestroyed + $shipsLost));
-
-        $iskDestroyed = getValue($row, 'iskDestroyed', $size);
-        $iskDestroyedRank = getValue($row, 'iskDestroyedRank', $size);
-        $iskLost = getValue($row, 'iskLost', $size);
-        $iskLostRank = getValue($row, 'iskLostRank', $size);
-        $iskEff = ($iskDestroyed / ($iskDestroyed + $iskLost));
-
-        $pointsDestroyed = getValue($row, 'pointsDestroyed', $size);
-        $pointsDestroyedRank = getValue($row, 'pointsDestroyedRank', $size);
-        $pointsLost = getValue($row, 'pointsLost', $size);
-        $pointsLostRank = getValue($row, 'pointsLostRank', $size);
-        $pointsEff = ($pointsDestroyed / ($pointsDestroyed + $pointsLost));
-
-        $avg = ceil(($shipsDestroyedRank + $iskDestroyedRank + $pointsDestroyedRank) / 3);
-        $adjuster = (1 + $shipsEff + $iskEff + $pointsEff) / 4;
-        $score = ceil($avg / $adjuster);
-
-        $mdb->getCollection('statistics')->update($row, ['$set' => ['overallScore' => (int) $score]]);
-    }
-
-    Util::out("$type Overall rank updating");
-    $currentRank = 0;
-    $result = $mdb->find('statistics', ['type' => $type], ['overallScore' => 1], null, ['months' => 0, 'groups' => 0]);
-    foreach ($result as $row) {
-        if (@$row['overallScore'] == null) {
-            $mdb->getCollection('statistics')->update($row, ['$unset' => ['overallRank' => 1]]);
-        } else {
-            ++$currentRank;
-            $mdb->getCollection('statistics')->update($row, ['$set' => ['overallRank' => $currentRank]]);
-            $mdb->insertUpdate('ranksProgress', ['type' => $type, 'id' => $row['id'], 'date' => $date], ['overallRank' => $currentRank]);
-        }
-    }
-    Util::out("Completed all time ranks for $type");
+	$avg = ceil(($shipsDestroyedRank + $iskDestroyedRank + $pointsDestroyedRank) / 3);
+	$adjuster = (1 + $shipsEff + $iskEff + $pointsEff) / 4;
+	$score = ceil($avg / $adjuster);
+	$redis->zAdd("tq:ranks:$type:score", $score, $id);
+	$mdb->set("statistics", $row, $update);
 }
 
-function getValue($array, $field, $default)
-{
-    $value = @$array[$field];
-    if (((int) $value) != 0) {
-        return $value;
-    }
+$count = 0;
+Util::out("All time ranks - third iteration");
+$iter = $information->find();
+while ($row = $iter->next()) {
+	$type = $row['type'];
+	$id = $row['id'];
+	$cleanup["tq:ranks:$type:score"] = 1;
+	$rank = 1 + $redis->zRank("tq:ranks:$type:score", $id);
+	$mdb->set("statistics", $row, ['overallRank' => $rank]);
+	$redis->hSet("tq:ranks:$type:alltime:$today", $id, $rank);
+	$expires[$type] = true;
+}
 
-    return $default;
+foreach ($expires as $type) $redis->expire("tq:ranks:$type:alltime:$today", (86400 * 15));
+foreach ($cleanup as $key) $redis->del($key);
+
+$redis->setex($todaysKey, 87000, true);
+
+function getValue(&$array, $index) {
+	$value = (int) @$array[$index];
+	return $value > 0 ? $value : 1000000000000;
 }
