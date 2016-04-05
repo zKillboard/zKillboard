9b542dbb1e50d9e2554f71634e7a0ef9101144ab
Clinton Weiss <cvweiss@gmail.com>
2016-04-05 14:13:50 +0000

    first step to displaying possible insurance on killmails

---
+++ cron/9.insurance.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once "../init.php";
+
+$date = (int) date('Ymd', time() - 7200);
+if ($redis->get("zkb:insuranceFetched:$date") == true) exit();
+
+$json = CrestTools::curlFetch("https://public-crest.eveonline.com/insuranceprices/");
+$insurance = json_decode($json, true);
+$items = isset($insurance['items']) ? $insurance['items'] : [];
+foreach ($items as $item) {
+	$typeID = $item['type']['id'];
+	$insert = ['typeID' => $typeID, 'date' => $date];
+	foreach ($item['insurance'] as $i) {
+		$insert[$i['level']] = ['cost' => round($i['cost']), 'payout' => round($i['payout'])];
+	}
+	$mdb->insertUpdate("insurance", ['typeID' => $typeID, 'date' => $date], $insert);
+}
+$redis->setex("zkb:insuranceFetched:$date", 86400, true);
