61393a9b13edc6485a49502eab67506b66244d44
Clinton Weiss <cvweiss@gmail.com>
2016-05-09 14:44:42 +0000

    api validation before putting into redis

--- cron/0.apiPopulate.php
+++ cron/0.apiPopulate.php
@@ -2,7 +2,7 @@
 
 require_once '../init.php';
 
-if (date('i') % 5  != 0) exit();
+//if (date('i') % 5  != 0) exit();
 
 $apis = $mdb->getCollection('apis');
 $tqApis = new RedisTimeQueue('tqApis', 9600);
@@ -11,5 +11,16 @@ $allApis = $mdb->find('apis');
 foreach ($allApis as $api) {
 	$errorCode = (int) @$api['errorCode'];
 	if (in_array($errorCode, [106, 203, 220, 222, 404, 522])) continue;
+
+	$keyID = $api['keyID'];
+	$vCode = $api['vCode'];
+
+        $keyID = (int) $keyID;
+        if ($keyID == 0) continue;
+
+        $before = $vCode;
+        $vCode = preg_replace( '/[^a-z0-9]/i', "", (string) $vCode);
+        if ($before != $vCode) continue;
+
 	$tqApis->add($api['_id']);
 }
