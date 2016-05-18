d81a4d2dc550a4225a11884cf1210317227338c1
Clinton Weiss <cvweiss@gmail.com>
2016-05-18 21:29:42 +0000

    sorting item table - round 1

--- public/js/rankstablesort.js
+++ public/js/rankstablesort.js
@@ -1,78 +1,98 @@
 $(document).ready(function() 
-    { 
-jQuery.extend( jQuery.fn.dataTableExt.oSort, {
-    "numeric-comma-pre": function ( a ) {
-		var a = a.replace( /<.*?>/g, "" );
-        var a = a.replace( /,/, "" );
-		var before = a;
-        var a = a.replace( /TTT/g, "00000000000000000" );
-        var a = a.replace( /TT/g, "000000000000000" );
-        var a = a.replace( /T/g, "000000000000" );
-        var a = a.replace( /B/g, "000000000" );
-        var a = a.replace( /M/g, "000000" );
-        var a = a.replace( /K/g, "000" );
-        var a = a.replace( /,/, "" );
-		var after = a;
-		if (before != after) a = a.replace('.', "");
-        return parseFloat( a );
-    },
- 
-    "numeric-comma-asc": function ( a, b ) {
-        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
-    },
- 
-    "numeric-comma-desc": function ( a, b ) {
-        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
-    }
-} );
+		{ 
+		jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+			"numeric-comma-pre": function ( a ) {
+			var a = a.replace( /<.*?>/g, "" );
+			var a = a.replace( /,/, "" );
+			var a = a.replace( /,/, "" );
+			var a = a.replace( /,/, "" );
+			var a = a.replace( /,/, "" );
+			var a = a.replace( /,/, "" );
+			var before = a;
+			var a = a.replace( /TTT/g, "00000000000000000" );
+			var a = a.replace( /TT/g, "000000000000000" );
+			var a = a.replace( /T/g, "000000000000" );
+			var a = a.replace( /B/g, "000000000" );
+			var a = a.replace( /M/g, "000000" );
+			var a = a.replace( /K/g, "000" );
+			var a = a.replace( /,/, "" );
+			var after = a;
+			if (before != after) a = a.replace('.', "");
+			return parseFloat( a );
+			},
 
-	$(".corpstats").dataTable({
-        "bPaginate": false,
-        "bLengthChange": false,
-        "bFilter": false,
-        "bSort": true,
-        "bInfo": false,
-        "bAutoWidth": false,
-		"aoColumns": [
-			{ "sType" : "string" },
-			{ "sType" : "string" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "string" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-		]	
-	});
-        $(".rank-table").dataTable( {
-        "bPaginate": false,
-        "bLengthChange": false,
-        "bFilter": false,
-        "bSort": true,
-        "bInfo": false,
-        "bAutoWidth": false,
-		"aoColumns": [
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "string" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-			{ "sType" : "numeric-comma" },
-		]
-    } );
-    } 
+			"numeric-comma-asc": function ( a, b ) {
+			return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+			},
+
+			"numeric-comma-desc": function ( a, b ) {
+				return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+			}
+		} );
+
+		$(".corpstats").dataTable({
+				"bPaginate": false,
+				"bLengthChange": false,
+				"bFilter": false,
+				"bSort": true,
+				"bInfo": false,
+				"bAutoWidth": false,
+				"aoColumns": [
+				{ "sType" : "string" },
+				{ "sType" : "string" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "string" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				]	
+				});
+		$(".rank-table").dataTable( {
+				"bPaginate": false,
+				"bLengthChange": false,
+				"bFilter": false,
+				"bSort": true,
+				"bInfo": false,
+				"bAutoWidth": false,
+				"aoColumns": [
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "string" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" },
+				]
+		} );
+		$(".item-table").dataTable( {
+				"bPaginate": false,
+				"bLengthChange": false,
+				"bFilter": false,
+				"bSort": true,
+				"bStateSave": false,
+				"bInfo": false,
+				"bAutoWidth": false,
+				"aaSorting": [],
+				"aoColumns": [
+				{ "sType" : "string" },
+				{ "sType" : "string" },
+				{ "sType" : "numeric-comma" },
+				{ "sType" : "numeric-comma" }
+				]
+		} );
+		} 
 );
