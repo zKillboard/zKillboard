f99532b01595d139e4344e617edaa15e916e29f1
Clinton Weiss <cvweiss@gmail.com>
2016-05-18 15:34:33 +0000

    properly iteratoe through items

--- cron/9.updateItems.php
+++ cron/9.updateItems.php
@@ -2,8 +2,6 @@
 
 require_once "../init.php";
 
-if (date('i') != 45) exit();
-
 if ($redis->get("tq:itemsPopulated") != true)
 {
         Util::out("Waiting for items to be populated...");
@@ -13,21 +11,16 @@ if ($redis->get("tq:itemsPopulated") != true)
 $assign = ['capacity', 'name', 'portionSize', 'mass', 'volume', 'description', 'radius', 'published'];
 $attrs = ['lowSlots', 'medSlots', 'hiSlots', 'rigSlots', 'techLevel', 'shieldCapacity', 'armorHP', 'hp'];
 
-$rows = $mdb->find("information", ['type' => 'typeID']);
+$hour24 = $mdb->now(-86400);
+$rows = $mdb->find("information", ['type' => 'typeID', 'lastCrestUpdate' => ['$lt' => $hour24]]);
 foreach ($rows as $row) {
 	$typeID = (int) $row['id'];
-	$lastCrestUpdate = @$row['lastCrestUpdate'];
-	if ($lastCrestUpdate != null && $lastCrestUpdate->sec > (time() - 86400)) continue;
 	$crest = CrestTools::getJSON("$crestServer/types/$typeID/");
 
 	foreach ($assign as $key) {
 		if (isset($crest[$key])) $row[$key] = $crest[$key];
 	}
 
-        unset($row['lowSlotCount']);
-        unset($row['midSlotCount']);
-        unset($row['highSlotCount']);
-        unset($row['rigSlotCount']);
         // Dogma
         if (isset($crest['dogma']['attributes'])) {
                 foreach ($crest['dogma']['attributes'] as $attribute) {
