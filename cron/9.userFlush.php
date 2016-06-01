5bd9ee9d511fdb93c4482d47f8b3521fd954ecfb
Clinton Weiss <cvweiss@gmail.com>
2016-06-01 02:54:11 +0000

    ensure user preferences that are stored in Redis are flushed to the db for backup

---
+++ cron/9.userFlush.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once "../init.php";
+
+$today = date('Ymd');
+$todaysKey = "RC:userFlush:$today";
+if ($redis->get($todaysKey) == true) exit();
+
+Util::out("Flushing user preferences to disk.");
+
+$keys = $redis->keys("user:*");
+foreach ($keys as $key)
+{
+	$userSettings = $redis->hGetAll($key);
+	$doc = $mdb->findDoc("users", ['userID' => $key]);
+	if (isset($doc['_id'])) $userSettings['_id'] = @$doc['_id'];
+	$userSettings['userID'] = $key;
+	$mdb->save("users", $userSettings);
+}
+$redis->setex($todaysKey, 86400, true);
