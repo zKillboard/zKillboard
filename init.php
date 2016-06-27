6782f4e7a2a91d146f6c5831b54566ef0448dff1
Clinton Weiss <cvweiss@gmail.com>
2016-06-27 19:48:38 +0000

    some composer cleanup

--- init.php
+++ init.php
@@ -18,20 +18,6 @@ if ($debug) {
 // vendor autoload
 require 'vendor/autoload.php';
 
-// zkb class autoloader
-spl_autoload_register('zkbautoload');
-
-function zkbautoload($class_name)
-{
-    $baseDir = dirname(__FILE__);
-    $fileName = "$baseDir/classes/$class_name.php";
-    if (file_exists($fileName)) {
-        require_once $fileName;
-
-        return;
-    }
-}
-
 $mdb = new Mdb();
 
 $redis = new Redis();
