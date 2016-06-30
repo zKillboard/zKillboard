8e5bd9c4d6cee8af5bef7288401df27b290f95c6
Clinton Weiss <cvweiss@gmail.com>
2016-06-30 15:47:28 +0000

    overhaul

--- cron/9.social.php
+++ cron/9.social.php
@@ -2,106 +2,43 @@
 
 require_once '../init.php';
 
-global $beSocial;
-if ($beSocial != true) 
-{
-	$redis->del("queueSocial");
-	exit();
-}
-
 $queueSocial = new RedisQueue('queueSocial');
-$timer = new Timer();
-while ($timer->stop() < 59000) {
-    $killID = $queueSocial->pop();
-    if ($killID != null) {
-        beSocial($killID);
-    }
+$minute = date('Hi');
+
+while ($beSocial && $minute == date('Hi')) {
+	$killID = $queueSocial->pop();
+	if ($killID != null) {
+		beSocial($killID);
+	}
 }
 
 function beSocial($killID)
 {
-    global $beSocial, $mdb;
+	global $mdb, $fullAddr, $twitterName;
 
-    if (!isset($beSocial)) {
-        $beSocial = false;
-    }
-    if ($beSocial === false) {
-        return;
-    }
+	$twitMin = 15000000000;
+	$kill = $mdb->findDoc('killmails', ['killID' => $killID]);
 
-    if ($killID < 0) {
-        return;
-    }
-    $ircMin = 10000000000;
-    $twitMin = 10000000000;
+	$hours24 = time() - 86400;
+	$victimInfo = $kill['involved'][0];
+	$totalPrice = $kill['zkb']['totalValue'];
+	if (@$kill['social'] == true || $kill['dttm']->sec < $hours24 || $victimInfo == null || $totalPrice < $twitMin) {
+		return;
+	}
 
-    $kill = $mdb->findDoc('killmails', ['killID' => $killID]);
+	Info::addInfo($victimInfo);
 
-    if (@$kill['social'] == true) {
-        return;
-    }
-    $hours24 = time() - 86400;
-    if ($kill['dttm']->sec < $hours24) {
-        return;
-    }
+	$url = "$fullAddr/kill/$killID/";
+	$message = $victimInfo['shipName'] . ' worth ' . Util::formatIsk($totalPrice) . " ISK was destroyed! $url";
 
-    // Get victim info
-    $victimInfo = $kill['involved'][0];
-    if ($victimInfo == null) {
-        return;
-    }
-    $totalPrice = $kill['zkb']['totalValue'];
+	$name = isset($victimInfo['characterName']) ? $victimInfo['characterName'] : $victimInfo['corporationName'];
+	$name = Util::endsWith($name, 's') ? $name . "'" : $name . "'s";
+	$message = "$name $message #tweetfleet #eveonline";
 
-    Info::addInfo($victimInfo);
+	$mdb->getCollection('killmails')->update(['killID' => $killID], ['$unset' => ['social' => true]]);
 
-    // Reduce spam of freighters and jump freighters
-    $shipGroupID = $victimInfo['groupID'];
-    if (in_array($shipGroupID, array(513, 902))) {
-        $shipPrice = Price::getItemPrice($victimInfo['shipTypeID'], date('Ymd'));
-        $ircMin += $shipPrice;
-        $twitMin += $shipPrice;
-    }
+	$message = strlen($message) > 120 ? str_replace(' worth ', ': ', $message) : $message;
+	$message = strlen($message) > 120 ? str_replace(' was destroyed!', '', $message) : $message;
 
-    $worthIt = false;
-    $worthIt |= $totalPrice >= $ircMin;
-    if (!$worthIt) {
-        return;
-    }
-
-    $tweetIt = false;
-    $tweetIt |= $totalPrice >= $twitMin;
-
-    global $fullAddr, $twitterName;
-    $url = "$fullAddr/kill/$killID/";
-
-    if ($url == '') {
-        $url = "$fullAddr/kill/$killID/";
-    }
-    $message = '|g|'.$victimInfo['shipName'].'|n| worth |r|'.Util::formatIsk($totalPrice)." ISK|n| was destroyed! $url";
-    if (!isset($victimInfo['characterName'])) {
-        $victimInfo['characterName'] = $victimInfo['corporationName'];
-    }
-    if (strlen($victimInfo['characterName']) < 25) {
-        $name = $victimInfo['characterName'];
-        if (Util::endsWith($name, 's')) {
-            $name .= "'";
-        } else {
-            $name .= "'s";
-        }
-        $message = "$name $message";
-    }
-    $mdb->getCollection('killmails')->update(['killID' => $killID], ['$unset' => ['social' => true]]);
-
-    $message = Log::stripIRCColors($message);
-
-    $message .= ' #tweetfleet #eveonline';
-    if (strlen($message) > 120) {
-        $message = str_replace(' worth ', ': ', $message);
-    }
-    if (strlen($message) > 120) {
-        $message = str_replace(' was destroyed!', '', $message);
-    }
-    if ($tweetIt && strlen($message) <= 120) {
-        $return = Twit::sendMessage($message);
-    }
+	return strlen($message) <= 120 ? Twit::sendMessage($message) : false;
 }
