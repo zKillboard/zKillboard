1d11699e1412c294615c05efb06ae033c6256dcf
Clinton Weiss <cvweiss@gmail.com>
2016-07-11 20:17:53 +0000

    send crest url quickly for processing, humans are slow

--- public/js/common.js
+++ public/js/common.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
-	//setTimeout(updateKillsLastHour, 60000);
+    //setTimeout(updateKillsLastHour, 60000);
 
-	// Check to see if the user has ads enabled
-	if ( $("iframe").length == 0 ) {
-		$("#adsensetop, #adsensebottom").html("<center><img src='/img/wreck.png'> <a href='/information/payments/'>Remove Ads?</a> <img src='/img/wreck.png'></center></br/>");
-		$("#adnag").hide();
-	}
-
-    if ($("[rel=tooltip]").length) {
-		$("[rel=tooltip]").tooltip({
-			placement: "bottom",
-			animation: false
-		});
+    // Check to see if the user has ads enabled
+    if ( $("iframe").length == 0 ) {
+        $("#adsensetop, #adsensebottom").html("<center><img src='/img/wreck.png'> <a href='/information/payments/'>Remove Ads?</a> <img src='/img/wreck.png'></center></br/>");
+        $("#adnag").hide();
     }
 
-	//
+    if ($("[rel=tooltip]").length) {
+        $("[rel=tooltip]").tooltip({
+            placement: "bottom",
+            animation: false
+        });
+    }
+
+    //
     $('.dropdown-toggle').dropdown();
     $("abbr.timeago").timeago();
     $(".alert").alert()
 
-    // Javascript to enable link to tab
-    var url = document.location.toString();
+        // Javascript to enable link to tab
+        var url = document.location.toString();
     if (url.match('#')) {
         $('.nav-pills a[href=#'+url.split('#')[1]+']').tab('show') ;
     }
@@ -30,84 +30,99 @@ $(document).ready(function() {
         window.location.hash = e.target.hash;
     })
 
-	// hide #back-top first
-	$("#back-top").hide();
+    // hide #back-top first
+    $("#back-top").hide();
 
-	// fade in #back-top
-	$(function () {
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 500) {
-				$('#back-top').fadeIn();
-			} else {
-				$('#back-top').fadeOut();
-			}
-		});
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 500) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
 
-		// scroll body to 0px on click
-		$('#back-top a').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 100);
-			return false;
-		});
-	});
-	
-	//add the autocomplete search thing
-	$('#searchbox').zz_search( function(data, event) { window.location = '/' + data.type + '/' + data.id + '/'; event.preventDefault(); } );
-	
-	//and for the tracker entity lookup
-	$('#addentitybox').zz_search( function(data) { 
-		$('#addentity input[name="entitymetadata"]').val(JSON.stringify(data));
-		$('#addentity input[name="addentitybox"]').val(data.name);
-		$('#addentity').submit();
-	});
+        // scroll body to 0px on click
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 100);
+            return false;
+        });
+    });
+
+    //add the autocomplete search thing
+    $('#searchbox').zz_search( function(data, event) { window.location = '/' + data.type + '/' + data.id + '/'; event.preventDefault(); } );
+
+    //and for the tracker entity lookup
+    $('#addentitybox').zz_search( function(data) { 
+        $('#addentity input[name="entitymetadata"]').val(JSON.stringify(data));
+        $('#addentity input[name="addentitybox"]').val(data.name);
+        $('#addentity').submit();
+    });
 
     // prevent firing of window.location in table rows if a link is clicked directly
-	$('.killListRow a').click(function(e) {
-		e.stopPropagation();
-	});
+    $('.killListRow a').click(function(e) {
+        e.stopPropagation();
+    });
 
-	$('a.openMenu').click(function(e){
-		$('.content').toggleClass('opened');
-		$('.mobileNav').toggleClass('opened');
-		e.preventDefault();
-	});
+    $('a.openMenu').click(function(e){
+        $('.content').toggleClass('opened');
+        $('.mobileNav').toggleClass('opened');
+        e.preventDefault();
+    });
 
-	// auto show comments tab on detail page
-	if(window.location.hash.match(/comment/)) {
-		$('a[href="#comment"]').tab('show');
-	}
+    // auto show comments tab on detail page
+    if(window.location.hash.match(/comment/)) {
+        $('a[href="#comment"]').tab('show');
+    }
 
-	if (top !== self) {
-		$("#iframed").modal('show');
-	}
+    if (top !== self) {
+        $("#iframed").modal('show');
+    }
+
+    $("#killmailurl").bind('paste', function(event) {
+        console.log(event);
+        setTimeout(sentCrestUrl, 1);
+    });
 });
 
 function updateKillsLastHour() {
-	$("#lasthour").load("/killslasthour/");
-	setTimeout(updateKillsLastHour, 60000);
+    $("#lasthour").load("/killslasthour/");
+    setTimeout(updateKillsLastHour, 60000);
 }
 
 function saveFitting(id) {
-		$('#modalMessageBody').html('Saving fit....');
-		$('#modalMessage').modal('show');
-	
-		var request = $.ajax({
-			url: "/ccpsavefit/" + id + "/",
-			type: "GET",			
-			dataType: "text"
-		});
+    $('#modalMessageBody').html('Saving fit....');
+    $('#modalMessage').modal('show');
 
-		request.done(function(msg) {
-			$('#modalMessageBody').html(msg);
-			$('#modalMessage').modal('show');
-		});
+    var request = $.ajax({
+        url: "/ccpsavefit/" + id + "/",
+        type: "GET",			
+        dataType: "text"
+    });
+
+    request.done(function(msg) {
+        $('#modalMessageBody').html(msg);
+        $('#modalMessage').modal('show');
+    });
 }
 
 $('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
 
 function hideSortStuff(doHide)
 {
-	if (doHide) $(".hide-when-sorted").hide();
-	else $(".hide-when-sorted").show();
+    if (doHide) $(".hide-when-sorted").hide();
+    else $(".hide-when-sorted").show();
+}
+
+function sentCrestUrl() {
+    str = $("#killmailurl").val();
+    strSplit = str.split("/");
+    killID = strSplit[4];
+    hash = strSplit[5];
+    a = ['/crestmail/', killID, '/', hash, '/'];
+    url = a.join('');
+    $.get(url);
 }
