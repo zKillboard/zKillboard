2c12cbd948a37c8b8039e3603cf71e90f46a5de4
Clinton Weiss <cvweiss@gmail.com>
2016-11-16 14:41:57 +0000

    reduced cache and code clenaup

--- cron/everyFifteen.php
+++ cron/everyFifteen.php
@@ -19,13 +19,5 @@ $p['kills'] = true;
 
 $redis->set('zkb:TopIsk', json_encode(Stats::getTopIsk(array('pastSeconds' => ($numDays * 86400), 'categoryID' => 6, 'limit' => 5))));
 
-function getStats($column)
-{
-    $result = Stats::getTop($column, ['isVictim' => false, 'pastSeconds' => 604800]);
-
-    return $result;
-}
-
 $redis->keys('*'); // Helps purge expired ttl's
-
 $redis->setex($key, 900, true);
