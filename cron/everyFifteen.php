fc3cde124015a9a61d4b6647dcb4862c46e35b07
Clinton Weiss <cvweiss@gmail.com>
2016-11-21 20:57:32 +0000

    move some calcs to a timed procedure

--- cron/everyFifteen.php
+++ cron/everyFifteen.php
@@ -19,5 +19,59 @@ $p['kills'] = true;
 
 $redis->set('zkb:TopIsk', json_encode(Stats::getTopIsk(array('pastSeconds' => ($numDays * 86400), 'categoryID' => 6, 'limit' => 5))));
 
+getTop('Top Characters', 'characterID');
+getTop('Top Corporations', 'corporationID');
+getTop('Top Alliances', 'allianceID');
+getTop('Top Ships', 'shipTypeID');
+getTop('Top Systems', 'solarSystemID');
+getTop('Top Locations', 'locationID');
+
+// Collect active PVP stats
+$types = ['characterID', 'corporationID', 'allianceID', 'shipTypeID', 'solarSystemID', 'regionID'];
+$activePvP = [];
+foreach ($types as $type) {
+    $result = Stats::getDistinctCount($type, []);
+    if ($result <= 1) {
+        continue;
+    }
+    $type = str_replace('ID', '', $type);
+    if ($type == 'shipType') {
+        $type = 'Ship';
+    } elseif ($type == 'solarSystem') {
+        $type = 'System';
+    } else {
+        $type = ucfirst($type);
+    }
+    $type = $type.'s';
+    $row['type'] = $type;
+    $row['count'] = $result;
+    $activePvP[] = $row;
+}
+$totalKills = $mdb->count('oneWeek');
+$activePvP[] = ['type' => 'Total Kills', 'count' => "$totalKills"];
+$redis->set('RC:activePvp', json_encode($activePvP));
+
+// Some cleanup
 $redis->keys('*'); // Helps purge expired ttl's
 $redis->setex($key, 900, true);
+
+function getTop($title, $type)
+{
+    global $redis;
+
+    $key = "RC:Cache:$title:$type";
+    $retVal = [];
+
+    $ids = $redis->zRange("tq:ranks:weekly:$type", 0, 10);
+    if (sizeof($ids) == 0) {
+        return [];
+    }
+    foreach ($ids as $id) {
+        $retVal[] = [$type => $id, 'kills' => $redis->zScore("tq:ranks:weekly:$type:shipsDestroyed", $id)];
+    }
+    Info::addInfo($retVal);
+    $retVal = ['type' => str_replace('ID', '', $type), 'title' => $title, 'values' => $retVal];
+    $redis->set($key, json_encode($retVal));
+
+    return $retVal;
+}
--- view/index.php
+++ view/index.php
@@ -1,6 +1,6 @@
 <?php
 
-global $mdb, $redis, $baseAddr;
+global $mdb, $redis;
 
 $page = 1;
 $pageTitle = '';
@@ -8,101 +8,23 @@ $pageType = 'index';
 $requestUriPager = '';
 $serverName = $_SERVER['SERVER_NAME'];
 
-$kills = [];
-$activePvP = [];
+$topPoints = array();
+$topIsk = json_decode($redis->get('zkb:TopIsk'), true);
+$topPods = array();
 
-//$userID = User::getUserID();
-if (false) { //$serverName != $baseAddr || $userID == 1633218082) {
-    try {
-        $numDays = 7;
-        $p = ['corporationID' => 869043665] ; //Subdomains::getSubdomainParameters($serverName);
-        $page = max(1, min(25, $page));
-        $p['page'] = $page;
-        $columnName = key($p);
-        $id = (int) reset($p);
-        if (sizeof($p) <= 1) {
-            $app->redirect($fullAddr, 302);
-        }
-        $topPoints = array();
-        $topPods = array();
-        $p['kills'] = true;
-        $p['pastSeconds'] = ($numDays * 86400);
-        $top = array();
-        $top[] = Info::doMakeCommon('Top Characters', 'characterID', Stats::getTop('characterID', $p));
-        $top[] = ($columnName != 'corporationID' ? Info::doMakeCommon('Top Corporations', 'corporationID', Stats::getTop('corporationID', $p)) : array());
-        $top[] = ($columnName != 'corporationID' && $columnName != 'allianceID' ? Info::doMakeCommon('Top Alliances', 'allianceID', Stats::getTop('allianceID', $p)) : array());
-        $top[] = Info::doMakeCommon('Top Ships', 'shipTypeID', Stats::getTop('shipTypeID', $p));
-        $top[] = Info::doMakeCommon('Top Systems', 'solarSystemID', Stats::getTop('solarSystemID', $p));
-        $top[] = Info::doMakeCommon('Top Locations', 'locationID', Stats::getTop('locationID', $p));
-        $requestUriPager = str_replace('ID', '', $columnName)."/$id/";
-        $p['limit'] = 5;
-        $topIsk = Stats::getTopIsk($p);
-        unset($p['pastSeconds']);
-        unset($p['kills']);
-        // get latest kills
-        $killsLimit = 50;
-        $p['limit'] = $killsLimit;
-        $kills = Kills::getKills($p);
-        $kills = Kills::mergeKillArrays($kills, array(), $killsLimit, $columnName, $id);
-        Info::addInfo($p);
-        $pageTitle = array();
-        foreach ($p as $key => $value) {
-            if (strpos($key, 'Name') !== false) {
-                $pageTitle[] = $value;
-            }
-        }
-        $pageTitle = implode(',', $pageTitle);
-        $pageType = 'subdomain';
-    } catch (Exception $ex) {
-        echo "<pre>";
-        print_r($ex);
-        echo "</pre>";
-    }
-} else {
+$top = array();
+$top[] = getTop('Top Characters', 'characterID');
+$top[] = getTop('Top Corporations', 'corporationID');
+$top[] = getTop('Top Alliances', 'allianceID');
+$top[] = getTop('Top Ships', 'shipTypeID');
+$top[] = getTop('Top Systems', 'solarSystemID');
+$top[] = getTop('Top Locations', 'locationID');
 
-    $topPoints = array();
-    $topIsk = json_decode($redis->get('zkb:TopIsk'), true);
-    $topPods = array();
+// get latest kills
+$kills = Kills::getKills(array('cacheTime' => 60, 'limit' => 50));
 
-    $top = array();
-    $top[] = getTop('Top Characters', 'characterID');
-    $top[] = getTop('Top Corporations', 'corporationID');
-    $top[] = getTop('Top Alliances', 'allianceID');
-    $top[] = getTop('Top Ships', 'shipTypeID');
-    $top[] = getTop('Top Systems', 'solarSystemID');
-    $top[] = getTop('Top Locations', 'locationID');
-
-    // get latest kills
-    $kills = Kills::getKills(array('cacheTime' => 300, 'limit' => 50));
-
-    // Collect active PVP stats
-    $types = ['characterID', 'corporationID', 'allianceID', 'shipTypeID', 'solarSystemID', 'regionID'];
-    $activePvP = json_decode($redis->get('RC:activePvp'));
-    if ($activePvP === null) {
-        $activePvP = [];
-        foreach ($types as $type) {
-            $result = Stats::getDistinctCount($type, []);
-            if ($result <= 1) {
-                continue;
-            }
-            $type = str_replace('ID', '', $type);
-            if ($type == 'shipType') {
-                $type = 'Ship';
-            } elseif ($type == 'solarSystem') {
-                $type = 'System';
-            } else {
-                $type = ucfirst($type);
-            }
-            $type = $type.'s';
-            $row['type'] = $type;
-            $row['count'] = $result;
-            $activePvP[] = $row;
-        }
-        $totalKills = $mdb->count('oneWeek');
-        $activePvP[] = ['type' => 'Total Kills', 'count' => "$totalKills"];
-        $redis->setex('RC:activePvp', 900, json_encode($activePvP));
-    }
-}
+// Collect active PVP stats
+$activePvP = json_decode($redis->get('RC:activePvp'));
 
 $app->render('index.html', array('topPods' => $topPods, 'topIsk' => $topIsk, 'topPoints' => $topPoints, 'topKillers' => $top, 'kills' => $kills, 'page' => $page, 'pageType' => $pageType, 'pager' => true, 'pageTitle' => $pageTitle, 'requestUriPager' => $requestUriPager, 'activePvP' => $activePvP));
 
@@ -115,19 +37,5 @@ function getTop($title, $type)
     if ($retVal != null) {
         return $retVal;
     }
-    $retVal = [];
-
-    $ids = $redis->zRange("tq:ranks:weekly:$type", 0, 10);
-    if (sizeof($ids) == 0) {
-        return [];
-    }
-    foreach ($ids as $id) {
-        $retVal[] = [$type => $id, 'kills' => $redis->zScore("tq:ranks:weekly:$type:shipsDestroyed", $id)];
-    }
-    Info::addInfo($retVal);
-    $retVal = ['type' => str_replace('ID', '', $type), 'title' => $title, 'values' => $retVal];
-    $redis->setex($key, 900, json_encode($retVal));
-
-    return $retVal;
-    //return ['type' => str_replace("ID", "", $type), 'title' => $title, 'values' => $retVal];
+    return [];
 }
