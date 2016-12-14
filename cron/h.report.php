f209f131abc188751e0711c355103d86818621a0
Clinton Weiss <cvweiss@gmail.com>
2016-12-14 19:23:44 +0000

    move from cached to permanent

--- cron/h.report.php
+++ cron/h.report.php
@@ -23,13 +23,13 @@ $redis->set('zkb:totalKills', $count);
 
 $parameters = ['groupID' => 30, 'isVictim' => false, 'pastSeconds' => (86400 * 90), 'nolimit' => true];
 $data['titans']['data'] = Stats::getTop('characterID', $parameters);
-$redis->setex('zkb:titans', 9600, serialize(Stats::getTop('characterID', $parameters)));
+$redis->set('zkb:titans', serialize(Stats::getTop('characterID', $parameters)));
 
 $parameters = ['groupID' => 659, 'isVictim' => false, 'pastSeconds' => (86400 * 90), 'nolimit' => true];
-$redis->setex('zkb:supers', 9600, serialize(Stats::getTop('characterID', $parameters)));
-
-$redis->setex($key, 3600, 1);
+$redis->set('zkb:supers', serialize(Stats::getTop('characterID', $parameters)));
 
 // Cleanup old tickets > 3 months old
 $time = time() - (86400 * 90);
 $mdb->getCollection('tickets')->remove(['dttm' => ['$lte' => $time]]);
+
+$redis->setex($key, 3600, 1);
