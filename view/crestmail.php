acf8d697712e2d5c2ae46f68b447b79137a21a70
Clinton Weiss <cvweiss@gmail.com>
2016-12-28 17:38:16 +0000

    log log log

--- view/crestmail.php
+++ view/crestmail.php
@@ -5,6 +5,7 @@ global $mdb;
 if ($killID > 0 && strlen($hash) == 40) {
     if (!$mdb->exists('crestmails', ['killID' => (int) $killID, 'hash' => $hash])) {
         Log::log("New CRESTmail $killID");
+        ZLog::add("New CRESTmail $killID", User::getUserID());
         $mdb->insertUpdate('crestmails', ['killID' => (int) $killID, 'hash' => $hash], ['processed' => false]);
     }
 }
