a090c6b48bc793865ae55f5d45879f6729a7782c
Clinton Weiss <cvweiss@gmail.com>
2017-01-22 04:24:49 +0000

    more consistent logging

--- view/crestmail.php
+++ view/crestmail.php
@@ -4,8 +4,10 @@ global $mdb;
 
 if ($killID > 0 && strlen($hash) == 40) {
     if (!$mdb->exists('crestmails', ['killID' => (int) $killID, 'hash' => $hash])) {
-        Log::log("New CRESTmail $killID");
-        ZLog::add("New CRESTmail $killID", User::getUserID());
+        $userID = User::getUserID();
+        $name = $userID > 0 ? Info::getInfoField('characterID', (int) $userID, 'name') : "?anonymous?";
+        Log::log("  1 kills added by $name (POST)");
+        ZLog::add("  1 kills added by $name (POST)", $userID);
         $mdb->insertUpdate('crestmails', ['killID' => (int) $killID, 'hash' => $hash], ['processed' => false]);
     }
 }
