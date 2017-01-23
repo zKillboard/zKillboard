afbdce6a30ed5f9fbf411bf4f067a8dd25d83275
Clinton Weiss <cvweiss@gmail.com>
2017-01-23 19:28:13 +0000

    ignore blueprint copies, blueprints before 21103361 did not report if they were a copy or original, therefore just ignore them too

--- cron/6.queueItemIndex.php
+++ cron/6.queueItemIndex.php
@@ -10,22 +10,30 @@ $minute = date('Hi');
 while ($minute == date('Hi')) {
     $killID = (int) $itemQueue->pop();
     if ($killID > 0) {
-        Util::out("Adding items for $killID");
         $killmail = $mdb->findDoc("rawmails", ['killID' => $killID]);
         updateItems($killID, $killmail['victim']['items']);
     } else exit();
 }
 
-function updateItems($killID, $items) {
+function updateItems($killID, $items, $inContainer = false) {
     global $mdb;
 
     foreach ($items as $item) {
         $typeID = (int) $item['itemType']['id'];
+        if ($typeID == 0) continue;
+
+        $name = Info::getInfoField('typeID', $typeID, 'name');
+        $isBlueprint = strpos($name, ' Blueprint') !== false;
+
+        if ($isBlueprint && $inContainer == true) continue;
+        if ($isBlueprint && $item['singleton'] != 0) continue;
+        if ($isBlueprint && $killID < 21103361) continue;
+
         if (!$mdb->exists('itemmails', ['killID' => $killID, 'typeID' => $typeID])) {
             $mdb->insert('itemmails', ['killID' => $killID, 'typeID' => $typeID]);
         }
         if (isset($items['items'])) {
-            updateItems($killID, $items['items']);
+            updateItems($killID, $items['items'], true);
         }
     }
 }
