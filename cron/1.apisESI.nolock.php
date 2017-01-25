7b9d5309f8145721d323c4810a44c2c7f426ef6e
Clinton Weiss <cvweiss@gmail.com>
2017-01-25 20:10:44 +0000

    prevent divide by 0

--- cron/1.apisESI.nolock.php
+++ cron/1.apisESI.nolock.php
@@ -23,7 +23,7 @@ if (date('i') == 22 || $esi->size() == 0) {
 $esiCalls = new RedisTtlCounter('ttlc:esiCalls', 10);
 $esiFailure = new RedisTtlCounter('ttlc:esiFailure', 300);
 
-$usleep = max(50000, min(1000000, floor((1 / ($esi->size() / 3600)) * 700000)));
+$usleep = max(50000, min(1000000, floor((1 / (($esi->size() + 1) / 3600)) * 700000)));
 $redirect = str_replace("/cron/", "/cron/logs/", __FILE__) . ".log";
 $minute = date('Hi');
 while ($minute == date('Hi')) {
