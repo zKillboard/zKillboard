790b95484c9dc68011d8a85dca00414af298dc02
Clinton Weiss <cvweiss@gmail.com>
2017-01-29 05:27:22 +0000

    complete rewrite, now uses async guzzle

--- cron/9.corporations.php
+++ cron/9.corporations.php
@@ -1,66 +1,88 @@
 <?php
 
-use cvweiss\redistools\RedisTimeQueue;
-use cvweiss\redistools\RedisTtlCounter;
-
 require_once '../init.php';
 
-$counter = 0;
+libxml_use_internal_errors(false);
+
+$curl = new \GuzzleHttp\Handler\CurlMultiHandler();
+$handler = \GuzzleHttp\HandlerStack::create($curl);
+$client = new \GuzzleHttp\Client(['connect_timeout' => 10, 'timeout' => 10, 'handler' => $handler]);
+
 $information = $mdb->getCollection('information');
+$queueCorps = new \cvweiss\redistools\RedisTimeQueue('tqCorporations', 86400);
+
+$query = ['type' => 'corporationID'];
+if (date ('i') != 30) {
+    $query['lastApiUpdate'] = ['$exists' => false];
+}
+$corps = $information->find($query)->sort(['lastApiUpdate' => 1]);
+foreach ($corps as $corp) {
+    if ($corp['id'] === (string) $corp['id'] || $corp['id'] <= 599999) {
+        $mdb->remove("information", $corp);
+        continue;
+    }
+    $queueCorps->add($corp['id']);
+}
+
 $minute = date('Hi');
-$xmlSuccess = new RedisTtlCounter('ttlc:XmlSuccess', 300);
-$xmlFailure = new RedisTtlCounter('ttlc:XmlFailure', 300);
+$count = 0;
+while ($minute == date('Hi') && ($id = $queueCorps->next()))
+{
+    $url = "https://api.eveonline.com/corp/CorporationSheet.xml.aspx?corporationID=$id";
+    $client->getAsync($url)->then(function($response) use ($redis, $mdb, $id, $queueCorps, &$count) {
+            $count--;
+            $raw = (string) $response->getBody();
+            updateCorp($mdb, $redis, $id, $raw, $queueCorps);
+            }, function($response) use ($queueCorps, $id, &$count) {
+            $count--;
+            $queueCorps->setTime($id, time() + 300);
+            });
 
-$queueCorps = new RedisTimeQueue('tqCorporations', 86400);
+    $count++;
+    do {
+        $curl->tick();
+    } while ($count > 30) ;
+    usleep(50000);
+}
+$curl->execute();
 
-$i = date('i');
-if ($i == 30) {
-    $corps = $information->find(['type' => 'corporationID']);
-    foreach ($corps as $corp) {
-        $queueCorps->add($corp['id']);
+function updateCorp($mdb, $redis, $id, $raw, $queueCorps) {
+    try {
+        $xml = simplexml_load_string($raw);
+        if ($xml === false) {
+            $queueCorps->setTime($id, time() + 300);
+        }
+
+        $corpInfo = $xml->result;
+
+        $row = $mdb->findDoc("information", ['type' => 'corporationID', 'id' => (int) $id]);
+        $ceoID = (int) $corpInfo->ceoID;
+
+        $updates = [];
+        compareAttributes($updates, "name", @$row['name'], (string) $corpInfo->corporationName);
+        compareAttributes($updates, "ticker", @$row['ticker'], (string) $corpInfo->ticker);
+        compareAttributes($updates, "ceoID", @$row['ceoID'], $ceoID);
+        compareAttributes($updates, "memberCount", @$row['memberCount'], (int) $corpInfo->memberCount);
+        compareAttributes($updates, "allianceID", @$row['allianceID'], (int) $corpInfo->allianceID);
+        compareAttributes($updates, "factionID", @$row['factionID'], (int) $corpInfo->factionID);
+
+        // Does the CEO exist in our info table?
+        $ceoExists = $mdb->count('information', ['type' => 'characterID', 'id' => $ceoID]);
+        if ($ceoExists == 0) {
+            $ceoName = (string) $corpInfo->ceoName;
+            $mdb->insertUpdate('information', ['type' => 'characterID', 'id' => $ceoID], ['name' => $ceoName, 'corporationID' => $id]);
+        }
+
+        $updates['lastApiUpdate'] = new MongoDate(time());
+        $mdb->set("information", ['type' => 'corporationID', 'id' => (int) $id], $updates);
+    } catch (Exception $ex) {
+        print_r($ex);
+        $queueCorps->setTime($id, time() + 300);
     }
 }
 
-while ($minute == date('Hi')) {
-    $id = $queueCorps->next();
-    if ($id == null) {
-        exit();
+function compareAttributes(&$updates, $key, $oAttr, $nAttr) {
+    if ($oAttr !== $nAttr) {
+        $updates[$key] = $nAttr;
     }
-    $row = $mdb->findDoc('information', ['type' => 'corporationID', 'id' => (int) $id]);
-
-    $updates = [];
-    if (!isset($row['memberCount']) || (isset($row['memberCount']) && $row['memberCount'] != 0)) {
-        $id = (int) $row['id'];
-        $raw = @file_get_contents("https://api.eveonline.com/corp/CorporationSheet.xml.aspx?corporationID=$id");
-        if ($raw != '') {
-            $xmlSuccess->add(uniqid());
-            ++$counter;
-            $xml = @simplexml_load_string($raw);
-            if ($xml != null) {
-                $corpInfo = $xml->result;
-                if (isset($corpInfo->ticker)) {
-                    $ceoID = (int) $corpInfo->ceoID;
-                    $ceoName = (string) $corpInfo->ceoName;
-                    $updates['ticker'] = (string) $corpInfo->ticker;
-                    $updates['ceoID'] = $ceoID;
-                    $updates['memberCount'] = (int) $corpInfo->memberCount;
-                    $updates['allianceID'] = (int) $corpInfo->allianceID;
-                    $updates['factionID'] = (int) @$corpInfo->factionID;
-                    if (!isset($row['name'])) {
-                        $updates['name'] = (string) $corpInfo->corporationName;
-                    }
-
-                    // Does the CEO exist in our info table?
-                    $ceoExists = $mdb->count('information', ['type' => 'characterID', 'id' => $ceoID]);
-                    if ($ceoExists == 0) {
-                        $mdb->insertUpdate('information', ['type' => 'characterID', 'id' => $ceoID], ['name' => $ceoName, 'corporationID' => $id]);
-                    }
-                }
-            }
-        } else {
-            $xmlFailure->add(uniqid());
-        }
-    }
-    $updates['lastApiUpdate'] = new MongoDate(time());
-    $mdb->insertUpdate('information', ['type' => 'corporationID', 'id' => (int) $row['id']], $updates);
 }
