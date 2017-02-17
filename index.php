af36fec61347c05fb1008986a78d88df185f192f
Clinton Weiss <cvweiss@gmail.com>
2017-02-17 21:43:58 +0000

    some anti-scraping stuff added

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ use cvweiss\redistools\RedisTtlCounter;
 $pageLoadMS = microtime(true);
 
 $uri = @$_SERVER['REQUEST_URI'];
+$isApiRequest = substr($uri, 0, 5) == "/api/";
+
 if ($uri == "/kill/-1/") {
     header("Location: /keepstar1.html");
     exit();
@@ -16,10 +18,11 @@ if (strpos($uri, "_detail") !== false) {
     exit();
 }
 // Check to ensure we have a trailing slash, helps with caching
-if (substr($uri, -1) != '/') {
+if (substr($uri, -1) != '/' && strpos($uri, 'ccpcallback') === false) {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET');
-    header("Location: $uri/");
+    if ($isApiRequest) header("HTTP/1.1 400 Fix your code to include the trailing slash '/'");
+    else header("Location: $uri/", true, 301);
     exit();
 }
 
@@ -41,19 +44,35 @@ $app = new \Slim\Slim($config);
 $ip = IP::get();
 
 // Must rate limit now apparently
-$isApiRequest = substr($uri, 0, 5) == "/api/";
-if ($isApiRequest) {
-    $ipKey = "ip:$ip:" . time();
-    $multi = $redis->multi();
-    $multi->incr($ipKey, 1);
-    $multi->expire($ipKey, 3);
-    $multi->exec();
+$ipKey = "ip:$ip:" . time();
+$multi = $redis->multi();
+$multi->incr($ipKey, 1);
+$multi->expire($ipKey, 3);
+$multi->exec();
 
-    if ($redis->get($ipKey) > 3) {
-        header('HTTP/1.1 400 Slow down.');
-        exit();
+$limit = $isApiRequest ? 10 : 3;
+$noLimits = ['/navbar/', '/post/', '/autocomplete/'];
+$noLimit = false;
+foreach ($noLimits as $noLimit) $noLimit |= (substr($uri, 0, strlen($noLimit)) === $noLimit);
+$count = $redis->get($ipKey);
+if ($noLimit === false  && $count >= $limit) {
+    header('HTTP/1.1 429 Too many requests.');
+    die("<html><head><meta http-equiv='refresh' content='1'></head><body>Rate limited.</body></html>");
+}
+
+// Some anti-scraping code, far from perfect though
+$badBots = ['mechanize', 'python', 'java'];
+if (!$isApiRequest) {
+    $userAgent = strtolower(@$_SERVER['HTTP_USER_AGENT']);
+    foreach ($badBots as $badBot) {
+        if ($userAgent == "" || $userAgent == "-" || strpos($userAgent, $badBot) !== false) {
+            header('HTTP/1.1 403 Not authorized.');
+            die("APIs are useful, skill up and use that instead.");
+        }
     }
-} else if (substr($uri, 0, 9) == "/sponsor/" || substr($uri, 0, 11) == '/crestmail/' || $uri == '/navbar/' || substr($uri, 0, 9) == '/account/' || $uri == '/logout/' || substr($uri, 0, 4) == '/ccp') {
+}
+
+if (substr($uri, 0, 9) == "/sponsor/" || substr($uri, 0, 11) == '/crestmail/' || $uri == '/navbar/' || substr($uri, 0, 9) == '/account/' || $uri == '/logout/' || substr($uri, 0, 4) == '/ccp') {
     // Session
     session_set_save_handler(new RedisSessionHandler(), true);
     session_cache_limiter('');
@@ -62,15 +81,10 @@ if ($isApiRequest) {
     session_start();
 }
 
-if ($isApiRequest) {
-    $apiR = new RedisTtlCounter('ttlc:apiRequests', 300);
-    $apiR->add(uniqid());
-} else if ($uri == '/navbar/') {
-    $nonApiR = new RedisTtlCounter('ttlc:nonApiRequests', 300);
-    $nonApiR->add(uniqid());
-    $uvisitors = new RedisTtlCounter('ttlc:unique_visitors', 300);
-    $uvisitors->add($ip);
-}
+$request = $isApiRequest ? new RedisTtlCounter('ttlc:apiRequests', 300) : new RedisTtlCounter('ttlc:nonApiRequests', 300);
+$request->add(uniqid());
+$uvisitors = new RedisTtlCounter('ttlc:unique_visitors', 300);
+$uvisitors->add($ip);
 
 $visitors = new RedisTtlCounter('ttlc:visitors', 300);
 $visitors->add($ip);
