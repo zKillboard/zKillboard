ec3087438585d0370254df7b1730c5c0853d1fc5
Clinton Weiss <cvweiss@gmail.com>
2017-02-26 03:57:43 +0000

    cleaned up the code a little

--- classes/Points.php
+++ classes/Points.php
@@ -20,14 +20,13 @@ class Points
             $itemInfo = Info::getInfo('typeID', $item['itemType']['id']);
             if (!@$itemInfo['fittable']) continue;
 
-            $flagName = Info::getFlagName($item['flag']);
             $typeID = $item['itemType']['id'];
             $qty = @$item['quantityDestroyed'] + @$item['quantityDropped'];
             $i = Info::getInfo('typeID', $typeID);
             $meta = 1 + floor(@$i['metaLevel'] / 2);
             $dangerFactor += isset($itemInfo['heatDamage']) * $qty * $meta; // offensive/defensive modules overloading are good for pvp
-            $dangerFactor += $itemInfo['groupID'] == 645 * $qty * $meta; // drone damange multipliers
-            $dangerFactor -= $itemInfo['groupID'] == 54 * $qty * $meta; // Mining ships don't earn as many points
+            $dangerFactor += ($itemInfo['groupID'] == 645) * $qty * $meta; // drone damange multipliers
+            $dangerFactor -= ($itemInfo['groupID'] == 54) * $qty * $meta; // Mining ships don't earn as many points
         }
         $points += $dangerFactor;
         $points *= max(0.01, min(1, $dangerFactor / 4));
@@ -49,7 +48,6 @@ class Points
         $modifier = min(1.2, max(0.8, $basePoints / $avg));
         $points = ceil($points * $modifier);
 
-        //echo "$killID " . $shipInfo['name'] . " $points\n";
         return $points;
     }
 }
