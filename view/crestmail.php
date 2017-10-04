15a29695249661196528dac8346f70de4e3f9699
Clinton Weiss <cvweiss@gmail.com>
2017-10-04 20:31:11 +0000

    only log if a new kill is actually added

--- view/crestmail.php
+++ view/crestmail.php
@@ -7,9 +7,13 @@ if ($killID > 0 && strlen($hash) == 40) {
         $userID = User::getUserID();
         $name = $userID > 0 ? Info::getInfoField('characterID', (int) $userID, 'name') : "?";
 
-        $r = $mdb->insert('crestmails', ['killID' => (int) $killID, 'hash' => $hash, 'added' => $mdb->now(), 'processed' => false]);
+        try { 
+            $mdb->insert('crestmails', ['killID' => (int) $killID, 'hash' => $hash, 'added' => $mdb->now(), 'processed' => false]);
+            ZLog::add("  1 kills added by $name (POST)", $userID, true);
+        } catch (Exception $ex) {
+            // ignore it
+        }
 
-        ZLog::add("  1 kills added by $name (POST)", $userID, true);
     }
 }
 header('HTTP/1.1 204 No Content');
