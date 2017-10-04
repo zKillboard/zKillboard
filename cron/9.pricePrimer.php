b999e6724f0d642d896043d909d257cde201f61d
Clinton Weiss <cvweiss@gmail.com>
2017-10-04 20:32:17 +0000

    Complete over haul of pre-fetching daily prices

--- cron/9.pricePrimer.php
+++ cron/9.pricePrimer.php
@@ -4,46 +4,75 @@ require_once '../init.php';
 
 global $primePrices;
 
-if ($primePrices != true) {
-    exit();
-}
+if ($primePrices != true) exit();
 
 $date = date('Ymd', time() - 7200);
 $yesterday = date('Y-m-d', time() - 7200 - 86400);
 $key = "tq:pricesChecked:$date";
-if ($redis->get($key) == true) {
+if ($redis->get($key) == "true") exit();
+
+Status::check('esi');
+$guzzler = new Guzzler(10, 10);
+$guzzler->call("https://esi.tech.ccp.is/v1/markets/groups/", "groupsSuccess", "fail");
+$guzzler->finish();
+
+$redis->setex($key, 86400, "true");
+
+function groupsSuccess($guzzler, $params, $content)
+{
+    $groups = json_decode($content, true);
+    foreach ($groups as $groupID) {
+        $guzzler->call("https://esi.tech.ccp.is/v1/markets/groups/$groupID/", "groupSuccess", "fail");
+    }  
+}
+
+function groupSuccess($guzzler, $params, $content)
+{
+    global $redis;
+
+    $group = json_decode($content, true);
+    $marketGroupID = $group['market_group_id'];
+    $types = $group['types'];
+
+    $today = date('Ymd', time());
+    $key = "tq:pricesFetched:$today";
+
+    foreach ($types as $typeID) {
+        $typeInfo = Info::getInfo("typeID", $typeID);
+        $name = strtolower($typeInfo['name']);
+
+        if ($typeInfo['published'] != true) continue;
+        if ($redis->hget($key, $typeID) == true) continue;
+
+        $guzzler->call("https://esi.tech.ccp.is/v1/markets/10000002/history/?type_id=$typeID", "typeHistorySuccess", "fail", ['typeID' => $typeID]);
+    }
+}
+
+function typeHistorySuccess($guzzler, $params, $content)
+{
+    global $mdb, $redis;
+
+    $type = json_decode($content, true);
+    $typeID = (int) $params['typeID'];
+    $row = $mdb->findDoc("prices", ['typeID' => $typeID]);
+    foreach ($type as $row) {
+        $avgPrice = $row['average'];
+        $date = $row['date'];
+        $row[$date] = $avgPrice;
+    }
+    $row['typeID'] = $typeID;
+
+    $mdb->save("prices", $row);
+
+    $today = date('Ymd', time());
+    $key = "tq:pricesFetched:$today";
+    $redis->hSet($key, $typeID, true);
+    $redis->expire($key, 86400);
+}
+
+function fail($guzzler, $params, $error)
+{
+    echo "Fail " . $params['uri'] . "\n";
+    $guzzler->finish();
     exit();
 }
-
-Status::check('crest');
-$crestPrices = CrestTools::getJson("$crestServer/market/prices/");
-if (!isset($crestPrices['items'])) {
-    exit();
-}
-
-foreach ($crestPrices['items'] as $item) {
-    Status::check('crest');
-    $typeID = $item['type']['id'];
-    $price = Price::getItemPrice($typeID, $date, true);
-
-    $marketHistory = $mdb->findDoc('prices', ['typeID' => $typeID]);
-    if ($marketHistory === null) {
-        $marketHistory = ['typeID' => $typeID];
-        $mdb->save('prices', $marketHistory);
-    }
-
-    $price = isset($item['averagePrice']) ? $item['averagePrice'] : (isset($item['adjustedPrice']) ? $item['adjustedPrice'] : 0);
-    if (!isset($marketHistory[$yesterday]) && $price > 0) {
-        $mdb->set('prices', ['typeID' => $typeID], [$yesterday => $price]);
-    }
-}
-
-$set = $mdb->find("information", ['type' => 'typeID'], ['id' => 1]);
-foreach ($set as $item) {
-    if (@$item['published'] == false) continue;
-    //if (@$item['marketable'] == false) continue;
-    $typeID = $item['id'];
-    $price = Price::getItemPrice($typeID, $date, true);
-}
-
-$redis->setex($key, 86400, true);
