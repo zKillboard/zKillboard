57536b221ea73e4171da9146fdd3bd626f334347
Clinton Weiss <cvweiss@gmail.com>
2017-10-19 20:59:37 +0000

    add flag for active users

--- view/overview.php
+++ view/overview.php
@@ -385,6 +385,8 @@ if ($pageType == 'overview' || $pageType == 'losses') {
 
 $extra['statsRecalced'] = $redis->llen('queueStats');
 
+$extra['recentkills'] = $type == 'character' && $redis->get("recentKillmailActivity:$id") == true;
+
 
 $renderParams = array('pageName' => $pageName, 'kills' => $kills, 'losses' => $losses, 'detail' => $detail, 'page' => $page, 'topKills' => $topKills, 'mixed' => $mixedKills, 'key' => $key, 'id' => $id, 'pageType' => $pageType, 'solo' => $solo, 'topLists' => $topLists, 'corps' => $corpList, 'corpStats' => $corpStats, 'summaryTable' => $stats, 'pager' => $hasPager, 'datepicker' => true, 'nextApiCheck' => $nextApiCheck, 'apiVerified' => $apiVerified, 'apiCorpVerified' => $apiCorpVerified, 'prevID' => $prevID, 'nextID' => $nextID, 'extra' => $extra, 'statistics' => $statistics, 'activePvP' => $activePvP, 'nextTopRecalc' => $nextTopRecalc, 'entityID' => $id, 'entityType' => $key, 'gold' => $gold);
 
