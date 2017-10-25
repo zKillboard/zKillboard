dc2dd74d75f53059bc168b5bd05e82a24f687383
Clinton Weiss <cvweiss@gmail.com>
2017-10-25 13:17:44 +0000

    new throttle method

--- classes/Status.php
+++ classes/Status.php
@@ -4,30 +4,30 @@ use cvweiss\redistools\RedisTtlCounter;
 
 class Status
 {
-    public static function addStatus($apiType, $success)
+    public static function addStatus($apiType, $success, $seconds = 300)
     {
         $apiType = strtolower($apiType);
         $status = $success == true ? "Success" : "Failure";
-        $rtc = new RedisTtlCounter("ttlc:{$apiType}{$status}", 300);
+        $rtc = new RedisTtlCounter("ttlc:{$apiType}{$status}", $seconds);
         $rtc->add(uniqid());
     }
 
-    public static function getStatus($apiType, $success)
+    public static function getStatus($apiType, $success, $seconds = 300)
     {
         $apiType = strtolower($apiType);
         $status = $success == true ? "Success" : "Failure";
-        $rtc = new RedisTtlCounter("ttlc:{$apiType}{$status}", 300);
+        $rtc = new RedisTtlCounter("ttlc:{$apiType}{$status}", $seconds);
         return $rtc->count();
     }
 
 
-    public static function check($apiType, $exitIfOffline = true, $exitIfFailure = true)
+    public static function check($apiType, $exitIfOffline = true, $exitIfFailure = true, $seconds = 300)
     {
         global $redis;
 
         $apiType = strtolower($apiType);
-        $rtcs = new RedisTtlCounter("ttlc:{$apiType}Success", 300);
-        $rtcf = new RedisTtlCounter("ttlc:{$apiType}Failure", 300);
+        $rtcs = new RedisTtlCounter("ttlc:{$apiType}Success", $seconds);
+        $rtcf = new RedisTtlCounter("ttlc:{$apiType}Failure", $seconds);
 
         $fail = false;
         $fail |= $rtcf->count() >= 100 && $exitIfFailure;
@@ -36,13 +36,13 @@ class Status
         if ($fail) exit();
     }
 
-    public static function checkStatus($guzzler = null, $apiType = '', $exitIfOffline = true, $exitIfFailure = true)
+    public static function checkStatus($guzzler = null, $apiType = '', $exitIfOffline = true, $exitIfFailure = true, $seconds = 300)
     {  
         global $redis;
 
         $apiType = strtolower($apiType);
-        $rtcs = new RedisTtlCounter("ttlc:{$apiType}Success", 300);
-        $rtcf = new RedisTtlCounter("ttlc:{$apiType}Failure", 300);
+        $rtcs = new RedisTtlCounter("ttlc:{$apiType}Success", $seconds);
+        $rtcf = new RedisTtlCounter("ttlc:{$apiType}Failure", $seconds);
 
         $fail = false;
         $fail |= $rtcf->count() >= 100 && $exitIfFailure;
@@ -52,5 +52,17 @@ class Status
             $guzzle = $guzzler == null ? null : $guzzler->finish();
             exit();
         }
+    }
+
+    public static function throttle($apiType, $perSecond, $seconds = 300)
+    {
+        $rtcs = new RedisTtlCounter("ttlc:{$apiType}Success", $seconds);
+        $rtce = new RedisTtlCounter("ttlc:{$apiType}Failure", $seconds);
+        $perSecond--;
+        do {
+            $sum = $rtcs->count() + $rtce->count();
+            $wait = $sum > ($perSecond * $seconds);
+            if ($wait) sleep(1);
+        } while ($wait);
     }
 }
