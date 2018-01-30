50ecb88fbf6d63d6b3c7270325bbafd934087889
Clinton Weiss <cvweiss@gmail.com>
2018-01-30 16:21:25 +0000

    fetch more market prices

---
+++ cron/9.priceFetcher.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once '../init.php';
+
+global $primePrices;
+
+if ($primePrices != true) exit();
+
+$date = date('Ymd', time() - 7200);
+$key = "tq:pricesFetched:$date";
+if ($redis->get($key) == "true") exit();
+
+Status::check('esi');
+$guzzler = new Guzzler(10, 10);
+$guzzler->call("https://esi.tech.ccp.is/latest/markets/prices/", "success", "fail");
+$guzzler->finish();
+
+$redis->setex($key, 86400, "true");
+
+function success($guzzler, $params, $content)
+{
+    global $mdb; 
+
+    $date = date('Y-m-d');
+    $json = json_decode($content, true);
+    foreach ($json as $segment) {
+        $typeID = $segment['type_id'];
+        $price = isset($segment['average_price']) ? $segment['average_price'] : $segment['adjusted_price'];
+        $row = $mdb->findDoc("prices", ['typeID' => $typeID]);
+        if (isset($row[$date])) continue;
+        $row[$date] = $price;
+        $mdb->save("prices", $row);
+    }
+}
+
+function fail($guzzler, $params, $error)
+{
+    Log::log("Failed to fetch prices...");
+    exit();
+}
