2ba9d3377b637d2eef269bc35bd1a4829c2b5b91
Clinton Weiss <cvweiss@gmail.com>
2018-04-14 05:59:52 +0000

    updated status check from crest to esi

--- cron/tqStatus.php
+++ cron/tqStatus.php
@@ -4,37 +4,57 @@ use cvweiss\redistools\RedisTtlCounter;
 
 require_once '../init.php';
 
-$serverStatus = "OFFLINE";
-$loggedIn = 0;
 $minute = date('Hi');
 if ($minute >= 1100 && $minute <= 1105) {
     $redis->set('tqStatus', 'OFFLINE'); // Just in case the result is cached on their end as online
     $redis->set('tqCount', 0);
 } else {
-    $root = CrestTools::getJSON($crestServer);
-    $version = @$root['serverVersion'];
-    if ($version != null) {
-        $redis->set('tqServerVersion', $version);
-    }
-
-
-    $serverStatus = $root == 0 ? 'UNKNOWN' : (isset($root['serviceStatus']) ? strtoupper($root['serviceStatus']) : 'OFFLINE');
-    $loggedIn = (int) @$root['userCount'];
-    $redis->set('tqCountInt', $loggedIn);
-    $loggedIn = $loggedIn == 0 ? $serverStatus : number_format($loggedIn, 0);
-
-    $redis->set('tqStatus', $serverStatus);
-    $redis->set('tqCount', $loggedIn);
+    $guzzler = new Guzzler();
+    $guzzler->call("$esiServer/v1/status/", "success", "fail");
+    $guzzler->finish();
 }
+
+$serverStatus = $redis->get("tqStatus");
+$loggedIn = $redis->get("tqCount");
 $killsLastHour = new RedisTtlCounter('killsLastHour', 3600);
 $killCount = number_format($killsLastHour->count(), 0);
 $redis->publish("public", json_encode(['action' => 'tqStatus', 'tqStatus' => $serverStatus, 'tqCount' => $loggedIn, 'kills' => $killCount]));
 
+$message = "";
 $message = apiStatus(null, 'esi', "Issues with CCP's ESI API - some killmails may be delayed.");
 $message = apiStatus($message, 'crest', "Issues with CCP's CREST API - some killmails may be delayed.");
 $message = apiStatus($message, 'xml', "Issues with CCP's XML API - some killmails may be delayed.");
 $message = apiStatus($message, 'sso', "Issues with CCP's SSO API - some killmails may be delayed.");
 $redis->setex('tq:apiStatus', 300, $message);
+
+function success($guzzler, $params, $content)
+{
+    global $redis;
+
+    $root = json_decode($content, true);
+    $version = $root['server_version'];
+    if ($version != null) {
+        $redis->set('tqServerVersion', $version);
+    }
+
+    $loggedIn = (int) @$root['players'];
+    $redis->set('tqCountInt', $loggedIn);
+
+    $serverStatus = $loggedIn > 100 ? 'ONLINE' : 'OFFLINE';
+    $loggedIn = $loggedIn == 0 ? $serverStatus : number_format($loggedIn, 0);
+
+    $redis->set('tqStatus', $serverStatus);
+    $redis->set('tqCount', $loggedIn);
+}
+
+function fail($guzzler, $params, $ex)
+{
+    global $redis;
+
+    $redis->set('tqStatus', 'UNKNOWN');
+    $redis->set('tqCount', 0);
+    $redis->set('tqCountInt', 0);
+}
 
 function apiStatus($prevMessage, $apiType, $notification)
 {
