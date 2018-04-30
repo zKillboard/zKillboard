d4b2c7b9739040b209e4c5de8d051ef16edb18d0
Clinton Weiss <cvweiss@gmail.com>
2018-04-30 15:26:21 +0000

    If one of the entities is closed, don't bother pulling that war info

--- cron/1.warConsumer.php
+++ cron/1.warConsumer.php
@@ -12,6 +12,11 @@ $queueWars = new RedisQueue('queueWars');
 if ($queueWars->size() == 0) {
     $wars = $mdb->getCollection('information')->find(['type' => 'warID', 'finished' => false])->sort(['id' => -1]);
     foreach ($wars as $war) {
+        $aMemberCount = isset($war['aggressor']['corporation_id']) ? Info::getInfoField("corporationID", $war['aggressor']['corporation_id'], "memberCount") : Info::getInfoField("allianceID", $war['aggressor']['alliance_id'], "memberCount");
+        $dMemberCount = isset($war['defender']['corporation_id']) ? Info::getInfoField("corporationID", $war['defender']['corporation_id'], "memberCount") : Info::getInfoField("allianceID", $war['defender']['alliance_id'], "memberCount");
+        if ($aMemberCount == 0 || $dMemberCount == 0) {
+            continue;
+        }
         $queueWars->push($war['id']);
     }
 }
