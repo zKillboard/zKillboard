11d08d944295af4325862f7a8c0422f8a0a6b308
Clinton Weiss <cvweiss@gmail.com>
2018-05-31 21:05:20 +0000

    show favorites even when not logged in just to annoy robbilie

--- view/favorites.php
+++ view/favorites.php
@@ -1,14 +1,14 @@
 <?php
 
-if (!User::isLoggedIn()) {
-    $app->redirect('/');
-    return;
-}
-$userID = (int) User::getUserID();
-
 global $mdb;
 
-$killIDs = $mdb->find("favorites", ['characterID' => (int) $userID], ['killID' => -1]);
-$kills = Kills::getDetails($killIDs);
+if (!User::isLoggedIn()) {
+    $kills = [];
+} else {
+    $userID = (int) User::getUserID();
+
+    $killIDs = $mdb->find("favorites", ['characterID' => (int) $userID], ['killID' => -1]);
+    $kills = Kills::getDetails($killIDs);
+}
 
 $app->render("favorites.html", ['kills' => $kills]);
