39861919ada0ed351d12460f47f2d1fc60752508
Clinton Weiss <cvweiss@gmail.com>
2018-06-03 20:53:09 +0000

    log favorite add/remove

--- view/favorite_modify.php
+++ view/favorite_modify.php
@@ -9,13 +9,16 @@ if (!User::isLoggedIn()) {
     return;
 }
 $userID = (int) User::getUserID();
+$name = Info::getInfoField("characterID", $userID, "name");
 
 $key =  ['characterID' => $userID, 'killID' => (int) $killID];
     $mdb->remove("favorites", $key);
 if ($action) {
     $mdb->insert("favorites", $key);
     echo json_encode(['color' => '#FDBC2C', 'message' => "Killmail has been added to your bookmarks."]);
+    Log::log("$name has favorited $killID");
 } else {
     $mdb->remove("favorites", $key);
     echo json_encode(['color' => 'rgb(128, 128, 128)', 'message' => "Killmail has been removed from your bookmarks."]);
+    Log::log("$name has unfavorited $killID");
 }
