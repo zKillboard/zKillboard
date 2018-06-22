301294e5744202fbe6b82aa5be147773ae79982b
Clinton Weiss <cvweiss@gmail.com>
2018-06-22 14:18:08 +0000

    clean up fitting scopes with no associated killmail scope for a character

---
+++ cron/9.scopeCleanup.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once "../init.php";
+
+if (date('i') != 0) exit();
+
+$rows = $mdb->find("scopes", ['scope' => 'esi-fittings.write_fittings.v1']);
+foreach ($rows as $row) {
+    $charID = $row['characterID'];
+    $kmRow = $mdb->findDoc("scopes", ['characterID' => $charID, 'scope' => 'esi-killmails.read_killmails.v1']);
+    if ($kmRow == null) {
+        Util::out("Removing fitting scope for $charID which has no killmail scope.");
+        $mdb->remove("scopes", $row);
+    }
+}
