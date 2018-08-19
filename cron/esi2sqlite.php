6a4eacef110a98065b14ee2786a26f5dea42429b
Clinton Weiss <cvweiss@gmail.com>
2018-08-19 21:19:07 +0000

    move esi mails to sqlite

---
+++ cron/esi2sqlite.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once "../init.php";
+
+$key = "zkb:esi2sqlite";
+if ($redis->get($key) == "true") exit();
+
+// initialize
+$db = new SQLite3("/home/kmstorage/sqlite/esi_killmails.sqlite");
+$db->busyTimeout(30000);
+
+$db->query("create table if not exists killmails (killmail_id integer primary key, mail blob)");
+$db->exec("begin transaction");
+
+$count = 0;
+$iter = $mdb->getCollection("esimails")->find()->sort(['killmail_id' => 1]);
+while ($iter->hasNext()) {
+    $next = $iter->next();
+    $killID = $next['killmail_id'];
+
+    $results = $db->query("select count(*) count from killmails where killmail_id = $killID");
+    $row = $results->fetchArray(SQLITE3_ASSOC);
+    if ($row['count'] > 0) {
+        Util::out("$killID exists");
+        $mdb->remove("esimails", $next);
+        continue;
+    }
+    Util::out("Inserting $killID");
+
+    unset($next['_id']);
+    $p = $db->prepare("insert or ignore into killmails (killmail_id, mail) values (:k, :m)");
+    $p->bindValue(":k", $killID);
+    $p->bindValue(":m", json_encode($next, JSON_UNESCAPED_SLASHES));
+    $r = $p->execute();
+    $p->close();
+
+    $count++;
+    if ($count % 10000 == 0) {
+        Util::out("$count $killID");
+        $db->exec("commit");
+        $db->exec("begin transaction");
+    }
+}
+$db->exec("commit");
+$db->close();
+
+$redis->setex($key, 3600, "true");
