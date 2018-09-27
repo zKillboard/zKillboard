b8f55c6044312dc45e9070e1a098868b439361d8
Clinton Weiss <cvweiss@gmail.com>
2018-09-27 15:26:30 +0000

    longer whitelist time

--- view/challenge.php
+++ view/challenge.php
@@ -5,5 +5,6 @@ global $redis, $ip;
 $uri = $redis->get("ip::redirect::$ip");
 if ($uri == "") $uri = "/";
 $redis->del("ip::redirect::$ip");
-$redis->setex("ip::challenge_safe::$ip", 300, "true");
+$redis->setex("ip::challenge_safe::$ip", 3600, "true");
 header("Location: $uri");
+Log::log("$ip successfully challenged and redirecting to $uri");
