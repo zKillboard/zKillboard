37ac9a2c2c96d82dc3eebd5934c26139594fd8ca
Clinton Weiss <cvweiss@gmail.com>
2019-01-13 19:47:27 +0000

    damn bots

--- view/kills.php
+++ view/kills.php
@@ -1,4 +1,6 @@
 <?php
 
+if ($page > 20) return $app->redirect("/kills/$type/page/20/", 302);
+
 $kills = KillCategories::getKills($type, $page);
 $app->render('kills.html', array('kills' => $kills, 'page' => $page, 'killsType' => $type, 'pager' => true));
