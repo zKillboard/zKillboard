c429795246d81efe1ccf4f77a82ae6278f996925
Clinton Weiss <cvweiss@gmail.com>
2019-01-13 19:47:45 +0000

    damn bots

--- view/api-kills.php
+++ view/api-kills.php
@@ -1,15 +1,20 @@
 <?php
 
+header('HTTP/1.1 202 Request being processed');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+$app->contentType('application/json; charset=utf-8');
+
+if ($page > 20) {
+    echo json_encode([]);
+    die();
+}
+
 $kills = KillCategories::getKills($type, $page, true);
 
 $killIDs = [];
 foreach ($kills as $id=>$kill) {
     $killIDs["$id"] = $kill['zkb']['hash'];
 }
-
-header('HTTP/1.1 202 Request being processed');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET');
-$app->contentType('application/json; charset=utf-8');
 
 echo json_encode($killIDs);
