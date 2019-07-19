bb20a054d5607c7a6df09db60237318d1d495ec5
Clinton Weiss <cvweiss@gmail.com>
2019-07-19 00:14:00 +0000

    be quiet

--- cron/9.topAllTime.php
+++ cron/9.topAllTime.php
@@ -31,7 +31,7 @@ function calcTop($row)
     if ($row['id'] == 0 || $row['type'] == null) return;
 
     $currentSum = (int) @$row['shipsDestroyed'];
-    Util::out("TopAllTime: " . $row['type'] . ' ' . $row['id'] . ' - ' . $currentSum);
+    //Util::out("TopAllTime: " . $row['type'] . ' ' . $row['id'] . ' - ' . $currentSum);
 
     $parameters = [$row['type'] => $row['id']];
     $parameters['limit'] = 100;
