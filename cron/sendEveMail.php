4599c551a020f3b42cbaf2003f70395674e2c977
Clinton Weiss <cvweiss@gmail.com>
2019-07-19 00:13:02 +0000

    slight modifications to how evemails are sent

--- cron/sendEveMail.php
+++ cron/sendEveMail.php
@@ -2,29 +2,26 @@
 
 require_once "../init.php";
 
-$minute = date('Hi');
-while ($minute == date('Hi')) {
-    $mail = $mdb->findDoc("evemails", ['sent' => false], ['_id' => -1]);
+$mails = $mdb->find("evemails", ['sent' => false], ['_id' => 1]);
+if (sizeof($mails)) {
+    $refreshToken = $mdb->findField("scopes", "refreshToken", ['characterID' => $evemailCharID, 'scope' => 'esi-mail.send_mail.v1']);
+    $accessToken = CrestSSO::getAccessToken($evemailCharID, null, $refreshToken);
+    if ($accessToken == null) {
+        Util::out("evemails to send, cannot obtain accessToken");
+        return;
+    }
 
-    if ($mail != null) {
-        $refreshToken = $mdb->findField("scopes", "refreshToken", ['characterID' => $evemailCharID, 'scope' => 'esi-mail.send_mail.v1']);
-        $accessToken = CrestSSO::getAccessToken($evemailCharID, null, $refreshToken);
-
-        if ($accessToken == null) {
-            Util::out("evemails to send, cannot obtain accessToken");
-            return;
-        }
-
+    foreach ($mails as $mail) {
         $charID = (int) $mail['recipients'][0]['recipient_id'];
         $name = Info::getInfoField('characterID', $charID, 'name');
 
         if ($redis->get("zkb:evemail:$charID" ) == "sent") {
-            $mail['sent'] = 'spam-prevention';
+            $mail['sent'] = false;
             $mail['error'] = null;
             $mdb->save("evemails", $mail);
             continue;
         }
-    
+
         ZLog::add("Sending evemail to $name", $charID); 
 
         $mail['approved_cost'] = 10000;
@@ -40,9 +37,8 @@ while ($minute == date('Hi')) {
         if (isset($json['error'])) {
             ZLog::add("Failed sending evemail to $name, http code: " . @$json['httpCode'], $charID);
         } else {
-            $redis->setex("zkb:evemail:$charID", 1800, "sent");
+            $redis->setex("zkb:evemail:$charID", 300, "sent");
         }
-        sleep(12);
+        sleep(13);
     }
-    sleep(1);
 }
