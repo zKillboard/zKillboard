e7d44589e760075401dd02ae73d0a4f49bfc1f6e
cvweiss <cvweiss@gmail.com>
2020-08-16 02:23:36 +0000

    throttled

--- cron/9.characters.nolock.php
+++ cron/9.characters.nolock.php
@@ -22,19 +22,25 @@ $minute = date('Hi');
 while ($minute == date('Hi')) {
     $id = (int) $chars->next();
     if ($id > 1) {
+        $guzzler->tick();
+        usleep(250000);
+        $guzzler->tick();
         $row = $mdb->findDoc("information", ['type' => 'characterID', 'id' => $id]);
+
+        //if (((int) @$row['lastApiUpdate']->sec) != 0) continue;
+
         $killmail = $mdb->findDoc("killmails", ['involved.characterID' => $id], ['killID' => -1]);
         $epoch = ($killmail == null ? 0 : $killmail['dttm']->sec);
 
         // Only update characters that have had a kill in the last year, the rest update only 5 times a year
-        if (isset($row['name']) && $row['name'] != '' && time() - $epoch > $oneYear && $id & 73 != $dayPrime) continue;
+        if (((int) @$row['lastApiUpdate']->sec) != 0 && time() - $epoch > $oneYear && $id & 73 != $dayPrime) continue;
 
         $url = "$esiServer/v4/characters/$id/";
         $params = ['mdb' => $mdb, 'redis' => $redis, 'row' => $row, 'rtq' => $chars];
-        $a = (isset($row['lastApiUpdate']) && $row['name'] != '')? ['etag' => true] : [];
+        $a = (isset($row['lastApiUpdate']) && $row['name'] != '')? [] /*['etag' => true]*/ : [];
         $guzzler->call($url, "updateChar", "failChar", $params, $a);
     }
-    if ($id == 0) {
+    if ($id <= 1) {
         $guzzler->tick();
         sleep(1);
     }
@@ -51,15 +57,16 @@ function failChar(&$guzzler, &$params, &$connectionException)
     $rtq = $params['rtq'];
 
     switch ($code) {
-        case 500:
-            // Do nothing, something wrong with the character on CCPs end
-            break;
         case 0: // timeout
+        case 500:
         case 502: // ccp broke something...
         case 503: // server error
         case 504: // gateway timeout
         case 200: // timeout...
-            $rtq->setTime($id, (time() - 86400) + rand(3600, 7200));
+            sleep(1);
+            $newTime = (time() - 86400) + rand(3600, 7200);
+            //Util::out("Newtime for $id : " . (time() - $newTime)); 
+            //$rtq->setTime($id, $newTime);
             break;
         case 420:
             $guzzler->finish();
