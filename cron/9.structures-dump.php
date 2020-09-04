8f170ddbfe520cd07df16b72504ccf83b5450fe7
cvweiss <cvweiss@gmail.com>
2020-09-04 00:20:09 +0000

    full output

--- cron/9.structures-dump.php
+++ cron/9.structures-dump.php
@@ -7,12 +7,7 @@ if ($redis->get("zkb:structure_api") == "true") exit();
 $structures = $mdb->find("structures", ['hasMatch' => true]);
 $output = [];
 foreach ($structures as $row) {
-    if (@$row['public'] == false && @$row['hasKills'] == false) {
-        $sysName = Info::getInfoField('solarSystemID', $row['solar_system_id'], 'name');
-        $row['name'] = str_replace($sysName, '???', $row['name']);
-        $row['solar_system_id'] = null;
-    }
-    $output[$row['structure_id']] = ['name' => $row['name'], 'solar_system_id' => $row['solar_system_id'], 'position' => $row['position']];
+    $output[$row['structure_id']] = ['structure_id' => $row['structure_id'], 'name' => $row['name'], 'solar_system_id' => $row['solar_system_id'], 'position' => $row['position']];
 }
 file_put_contents("./public/api/structures.json", json_encode($output));
 
