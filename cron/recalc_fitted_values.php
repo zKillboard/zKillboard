59604406fcb507afbd52510904003f6e895f6a4f
cvweiss <cvweiss@gmail.com>
2021-05-22 18:43:52 +0000

    recalculating fitted values of all ships

---
+++ cron/recalc_fitted_values.php
@@ -0,0 +1,48 @@
+<?php
+
+$pid = pcntl_fork();
+
+require_once "../init.php";
+
+$fittedArray = [11, 12, 13, 87, 89, 93, 158, 159, 172, 2663, 3772];
+
+$minute = date("Hi");
+while ($minute == date("Hi")) {
+    $killID = (int) $redis->spop("fittedSet");
+    if ($killID <= 0) exit();
+
+    $mail = $mdb->findDoc("esimails", ['killmail_id' => $killID]);
+    $km = $mdb->findDoc("killmails", ['killID' => $killID]);
+
+    $date = substr($mail['killmail_time'], 0, 19);
+    $date = str_replace('.', '-', $date);
+
+    $shipValue = Price::getItemPrice($mail['victim']['ship_type_id'], $date);
+    $fittedValue = getFittedValue($killID, $mail['victim']['items'], $date);
+    $fittedValue += $shipValue;
+    if ($fittedValue != $km['zkb']['fittedValue']) {
+        //Log::log($killID . ": " . number_format($fittedValue, 2));
+        $mdb->getCollection("killmails")->update(['killID' => $killID], ['$set' => ['zkb.fittedValue' => $fittedValue]]);
+    }
+}
+
+function getFittedValue($killID, $items, $dttm)
+{
+    global $fittedArray;
+
+    $fittedValue = 0;
+    foreach ($items as $item) {
+        $typeID = (int) $item['item_type_id'];
+        if ($typeID == 0) continue;
+
+        $infernoFlag = Info::getFlagLocation($item['flag']);
+        $add = in_array($infernoFlag, $fittedArray);
+        if ($add) {
+            $qty = ((int) @$item['quantity_dropped'] + (int) @$item['quantity_destroyed']);
+            $price = Price::getItemPrice($typeID, $dttm);
+            $fittedValue += ($qty * $price);
+        }
+    }
+    return $fittedValue;
+}
+
