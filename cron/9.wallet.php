a9d96044d361a95f29bd278bb0710a5c157499ef
cvweiss <cvweiss@gmail.com>
2021-10-29 02:13:17 +0000

    fixed sendMail call

--- cron/9.wallet.php
+++ cron/9.wallet.php
@@ -96,7 +96,7 @@ function applyBalances()
 
                 ZLog::add("$months month" . ($months == 1 ? "" : "s")  . " of ad free time has been given to $charName from $amount ISK.", $charID);
                 User::sendMessage("Thank you for your payment of $amount ISK. $months month" . ($months == 1 ? "" : "s")  . " of ad free time has been given to $charName", $charID);
-                Util::sendsendEveMail($charID, "$shortAmount ISK Received", "Thank you for your payment of $amount ISK. $months months of ad free time has been given to $charName.\n\n<a href=\"https://zkillboard.com/character/$charID/\">Your zKillboard character page.</a>");
+                Util::sendEveMail($charID, "$shortAmount ISK Received", "Thank you for your payment of $amount ISK. $months months of ad free time has been given to $charName.\n\n<a href=\"https://zkillboard.com/character/$charID/\">Your zKillboard character page.</a>");
             }
         }
         $mdb->set('payments', $row, ['paymentApplied' => 1]);
