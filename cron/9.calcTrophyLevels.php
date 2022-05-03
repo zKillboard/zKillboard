441adb5585c7442cf9f674175cf2763ed18147a8
cvweiss <cvweiss@gmail.com>
2022-05-03 12:06:49 +0000

    calc trophies once a day

--- cron/9.calcTrophyLevels.php
+++ cron/9.calcTrophyLevels.php
@@ -4,9 +4,13 @@ require_once "../init.php";
 
 if ($redis->get("tobefetched") > 1000) exit();
 
-if (date('i') != 33) exit(); // Only do calcs once an hour
+if (date('Hi') < "1100") exit();
+$key = "zkb:calcedTrophies:" . date("Ymd");
+if ($redis->get("$key") == "true") exit();
 
-while (true) {
+$minute = Date('Hi');
+while ($minute == date('Hi')) {
+    if ($redis->get("zkb:reinforced") == true) exit();
     $row = $mdb->findDoc("statistics", ['calcTrophies' => true]);
     if ($row == null) break;
     $charID = $row['id'];
@@ -17,3 +21,5 @@ while (true) {
     $mdb->removeField("statistics", $row, 'calcTrophies');
 }
 
+$count = $mdb->count("statistics", ['calcTrophies' => true]);
+if ($count == 0) $redis->setex($key, 86400, "true");
