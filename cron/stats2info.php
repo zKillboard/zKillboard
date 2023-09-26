97cec239c8cbd4a58e5b59a551721ac386cd6b3b
cvweiss <cvweiss@gmail.com>
2023-09-26 02:50:47 +0000

    ensuring info records are present

---
+++ cron/stats2info.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once "../init.php";
+
+use cvweiss\redistools\RedisTimeQueue;
+
+if (date("i") != 0) exit();
+
+$count = 0;
+$cursor = $mdb->getCollection("statistics")->find()->sort(['_id' => -1]);
+while ($cursor->hasNext()) {
+    $count++;
+    $row = $cursor->next();
+    $type = $row['type'];
+    $id = $row['id'];
+    if (!in_array($type, ['characterID', 'corporationID', 'allianceID'])) continue;
+    $infoRow = $mdb->findDoc("information", ['type' => $type, 'id' => $id]);
+    if ($infoRow == null) {
+        Util::out("$type $id");
+        $defaultName = "$type $id";
+        $row = ['type' => $type, 'id' => (int) $id];
+        $mdb->insertUpdate('information', $row, ['name' => $defaultName]);
+        $rtq = new RedisTimeQueue("zkb:$type", 86400);
+        $rtq->add($id);
+        $count = 0;
+    }
+    if ($count > 100000) break;
+}
