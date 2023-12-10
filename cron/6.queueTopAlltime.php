d201202b99ddc025677cb2497725ccaf71cd24d8
cvweiss <cvweiss@gmail.com>
2023-12-10 16:10:15 +0000

    execute only once per day

--- cron/6.queueTopAlltime.php
+++ cron/6.queueTopAlltime.php
@@ -9,18 +9,20 @@ require_once '../init.php';
 if ($redis->get("tobefetched") > 1000) exit();
 if ($redis->get("zkb:reinforced") == true) exit();
 if ($redis->scard("queueStatsSet") > 1000) exit();
-if ($redis->get("zkb:topAllTimeComplete") == "true") exit();
+$todaysKey = "zkb:topAllTimeComplete:" . date('Ymd', time() - 36000);
 
 MongoCursor::$timeout = -1;
 $minute = date("Hi");
 $queueTopAllTime = new RedisQueue("queueTopAllTime");
 
-if ($master == true && $queueTopAllTime->size() == 0 && $mdb->findDoc("statistics", ['reset' => true]) == null) {
+if ($master == true && $redis->get($todaysKey) != "true" && $queueTopAllTime->size() == 0 && $mdb->findDoc("statistics", ['reset' => true]) == null) {
+    Util::out("Populating top all time queue");
     $cursor = $mdb->getCollection("statistics")->find(['calcAlltime' => true])->limit(5000);
     while ($cursor->hasNext()) {
         $row = $cursor->next();
         $queueTopAllTime->push(['type' => $row['type'], 'id' => (int) $row['id']]);
     }
+    $redis->setex($todaysKey, 86400, "true");
 }
 
 while ($queueTopAllTime->size() > 0 && date('Hi') == $minute) {
@@ -32,10 +34,6 @@ while ($queueTopAllTime->size() > 0 && date('Hi') == $minute) {
 
 $a = 0;
 while (pcntl_wait($a) > 0) sleep(1);
-if ($master && $queueTopAllTime->size() == 0 && $mdb->count("statistics", ['calcAllTime' => true]) == 0) {
-    Util::out("Completed topAllTime, next iteration in an hour.");
-    $redis->setex("zkb:topAllTimeComplete", 3600, "true"); 
-}
 exit();
 
 function calcTop($row)
