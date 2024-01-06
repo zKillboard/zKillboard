71c0c8ff992f39b4c4ecaa485b3cc281882831ce
cvweiss <cvweiss@gmail.com>
2024-01-06 18:37:25 +0000

    Added a couple of new comments

--- classes/Comments.php
+++ classes/Comments.php
@@ -3,10 +3,27 @@
 class Comments
 {
     /* Do not change the ordering of these comments */
-    public static $defaultComments = ['DUNKED', 'another one', 'nothing of value was lost', 
-                                        'memed', 'baited on a free ____', 'it would have lived if it were a shield super', 'RMT', 'wrong rigs', 'WTF?',
-                                        '🎺 ', 'meh', 'already replaced',
-                                        'loot fairy said no', 'press F to pay respects', 'good fight!', 'needs more purple',
-                                        '💩', 'The beacon provides...', 'I was drunk',
-                                    ];
+    public static $defaultComments = [
+        'DUNKED',
+        'another one',
+        'nothing of value was lost',
+        'memed',
+        'baited on a free ____',
+        'it would have lived if it were a shield super',
+        'RMT',
+        'wrong rigs',
+        'WTF?',
+        '🎺 ',
+        'meh',
+        'already replaced',
+        'loot fairy said no',
+        'press F to pay respects',
+        'good fight!',
+        'needs more purple',
+        '💩',
+        'The beacon provides...',
+        'I was drunk',
+        'calm down miner',
+        'the attackers can go ----',
+    ];
 }
