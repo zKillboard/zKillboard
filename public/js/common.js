54a182012d7cb1db9ed7658d0b46ef0f74d07e43
cvweiss <cvweiss@gmail.com>
2025-05-26 21:13:59 +0000

    should have been included in earlier commit, live updating of messages and showing markee banner for those with adblock

--- public/js/common.js
+++ public/js/common.js
@@ -83,11 +83,7 @@ function startWebSocket() {
         };
         ws.onopen = function(event) {
             pubsub('public');
-            // If we connected and somehow got completely disconnected - reload the page
-            ws.onclose = function(event) {
-                //window.location = window.location;
-            }
-        }
+        };
 
         var channel = entityType + ":" + entityID;
         if (entityPage != 'index' && entityPage != 'overview') channel = channel + ":" + entityPage;
@@ -158,6 +154,10 @@ function wslog(msg)
     } else if (json.action == 'statsbox') {
         console.log(json);
         statsboxUpdate(json);
+    } else if (json.action == 'message') {
+        console.log(json);
+        if (json.message.length > 0) $("#zkb-message").html("<center>" + json.message + "</center>").removeClass('hide');
+        else $("#zkb-message").html('').addClass('hide');
     } else {
         console.log("Unknown action: " + json.action);
     }
@@ -475,7 +475,10 @@ function adblockloaded() {
 function showAdblockedMessage() {
     if ($("#publifttop").html() == "") {
         gtag('event', 'adblocked', 'detectAdblock blocked');
-        $("#publifttop").html('<h4>AdBlocker Detected! :(</h4><p>Please support zKillboard by disabling your adblocker.<br/><a href="/information/payments/">Or block them with ISK and get a golden wreck too.</a></p>');
+        let html = '';
+        if (promoURI != '') html = `<div style='max-height: 130px; max-width: 100%;'><a href="${promoURI}" target="_blank"><img style='max-height: 130px; max-width: 100%;' src="${promoImage1}" alt="Promotional Image" />User code "zkill" for 3% Off!</a></div>`;
+        else html = '<h4>AdBlocker Detected! :(</h4><p>Please support zKillboard by disabling your adblocker.<br/><a href="/information/payments/">Or block them with ISK and get a golden wreck too.</a></p>';
+        $("#publifttop").html(html);
         if (ws) ws.close();
     }
 }
