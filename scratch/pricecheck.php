43285d62930047dc5d3936e2faef39d5a2491a63
cvweiss <cvweiss@gmail.com>
2025-08-08 23:16:41 +0000

    easy way to pricecheck an item

--- scratch/pricecheck.php
+++ scratch/pricecheck.php
@@ -2,5 +2,7 @@
 
 require_once "../init.php";
 
-echo Price::getItemPrice(42124, "2024-09-17", true, true);
+if (!isset($argv[1]) || !isset($argv[2])) die("use typeID yyyy-mm-dd as parameters...\n"); 
+
+echo Price::getItemPrice($argv[1], $argv[2], true, true);
 echo "\n";
