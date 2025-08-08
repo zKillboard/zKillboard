d0e93d6ef7fbbbdef63697c482eaac9fd3a59c96
cvweiss <cvweiss@gmail.com>
2025-08-08 16:04:17 +0000

    avoid div/0 error

--- view/typeRanks.php
+++ view/typeRanks.php
@@ -50,19 +50,22 @@ foreach ($r as $row) {
     $row['sdRank'] = Util::rankCheck($redis->zRevRank("tq:ranks:$pageEpoch:$column:shipsDestroyed$s", $id));
     $row['shipsLost'] = $redis->zScore("tq:ranks:$pageEpoch:$column:shipsLost$s", $id);
     $row['slRank'] = Util::rankCheck($redis->zRevRank("tq:ranks:$pageEpoch:$column:shipsLost$s", $id));
-    $row['shipEff'] = ($row['shipsDestroyed'] / ($row['shipsDestroyed'] + $row['shipsLost'])) * 100;
+    if ($row['shipsDestroyed'] + $row['shipsLost'] == 0) $row['shipEff'] = 0;
+    else $row['shipEff'] = ($row['shipsDestroyed'] / ($row['shipsDestroyed'] + $row['shipsLost'])) * 100;
 
     $row['iskDestroyed'] = $redis->zScore("tq:ranks:$pageEpoch:$column:iskDestroyed$s", $id);
     $row['idRank'] = Util::rankCheck($redis->zRevRank("tq:ranks:$pageEpoch:$column:iskDestroyed$s", $id));
     $row['iskLost'] = $redis->zScore("tq:ranks:$pageEpoch:$column:iskLost$s", $id);
     $row['ilRank'] = Util::rankCheck($redis->zRevRank("tq:ranks:$pageEpoch:$column:iskLost$s", $id));
-    $row['iskEff'] = ($row['iskDestroyed'] / ($row['iskDestroyed'] + $row['iskLost'])) * 100;
+    if ($row['shipEff'] == 0) $row['iskEff'] = 0;
+    else $row['iskEff'] = ($row['iskDestroyed'] / ($row['iskDestroyed'] + $row['iskLost'])) * 100;
 
     $row['pointsDestroyed'] = $redis->zScore("tq:ranks:$pageEpoch:$column:pointsDestroyed$s", $id);
     $row['pdRank'] = Util::rankCheck($redis->zRevRank("tq:ranks:$pageEpoch:$column:pointsDestroyed$s", $id));
     $row['pointsLost'] = $redis->zScore("tq:ranks:$pageEpoch:$column:pointsLost$s", $id);
     $row['plRank'] = Util::rankCheck($redis->zRevRank("tq:ranks:$pageEpoch:$column:pointsLost$s", $id));
-    $row['pointsEff'] = ($row['pointsDestroyed'] / ($row['pointsDestroyed'] + $row['pointsLost'])) * 100;
+    if ($row['shipEff'] == 0) $row['pointsEff'] = 0;
+    else $row['pointsEff'] = ($row['pointsDestroyed'] / ($row['pointsDestroyed'] + $row['pointsLost'])) * 100;
 
     $result[] = $row;
 }
