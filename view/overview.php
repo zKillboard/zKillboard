97ecbf5d241d25c0875c92732c960ab7415bc577
cvweiss <cvweiss@gmail.com>
2025-08-12 19:42:29 +0000

    ...

--- view/overview.php
+++ view/overview.php
@@ -16,7 +16,7 @@ if ($key != 'label' && (int) $id == 0) {
     }
     $id = $mdb->findField('information', 'id', ['type' => "${searchKey}ID", 'name' => $id]);
     if ($id > 0) return $app->redirect("/$key/$id/");
-    return $app->("./../");
+    return $app->redirect("./../");
 }
 
 if (strlen("$id") > 11) {
