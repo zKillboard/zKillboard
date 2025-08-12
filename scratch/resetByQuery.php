87db17d8af0388140d7d3d9285aef8524789fa43
cvweiss <cvweiss@gmail.com>
2025-08-12 20:46:03 +0000

    resetByQuery using new stats stop method

--- scratch/resetByQuery.php
+++ scratch/resetByQuery.php
@@ -2,10 +2,13 @@
 
 require_once "../init.php";
 
-$rows = $mdb->find("esimails", ['victim.items.item_type_id' => 74523, 'killmail_id' => ['$gt' => 120000000]]);
+$rows = $mdb->find("esimails", ['victim.items.item_type_id' => ['$in' => [81976, 82317, 82316, 82318, 82319, 81975, 81977, 81978]], 'killmail_id' => ['$lte' => 120000000]]);
 
+$redis->set("zkb:statsStop", "true");
+sleep(60);
 foreach ($rows as $row) {
     $killID = (int) $row['killmail_id'];
-    echo "$killID\n";
-    //Killmail::deleteKillmail($killID);
+//    echo "$killID\n";
+//    Killmail::deleteKillmail($killID);
 }
+$redis->del("zkb:statsStop");
