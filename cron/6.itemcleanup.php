3790d462315f6163939f096a0e6f86b893bf13cc
cvweiss <cvweiss@gmail.com>
2025-10-04 14:27:45 +0000

    "Bulk" implementation

--- cron/6.itemcleanup.php
+++ cron/6.itemcleanup.php
@@ -2,7 +2,7 @@
 
 require_once "../init.php";
 
-if (date('Hi') != "1100") exit();
+if (date('Hi') != "1000") exit();
 
 MongoCursor::$timeout = -1;
 
@@ -14,14 +14,18 @@ foreach ($typeIDs as $typeID) {
     $iter = $mdb->getCollection("itemmails")->find(['typeID' => $typeID])->sort(['killID' => -1]);
     $count = 0;
     $removed = 0;
+    $remove_queue = [];
     while ($iter->hasNext()) {
         $row = $iter->next();
         $count++;
         if ($count > 50) {
-            $mdb->remove("itemmails", $row);
+            $remove_queue[] = $row['killID'];
             $removed++;
         }
     }
+    
+    if (sizeof($remove_queue) > 0) $mdb->set("itemmails", ['killID' => ['$in' => $remove_queue]], ['remove' => true]);
     $current++;
     if ($removed > 0) Util::out("($current/$total) $typeID - $name removed: $removed");
 }
+$mdb->remove("itemmails", ['remove' => true]);
