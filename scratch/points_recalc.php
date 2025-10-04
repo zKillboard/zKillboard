c7deb6eed3c5846778f7e0ae76deeec00de5450d
cvweiss <cvweiss@gmail.com>
2025-10-04 14:29:11 +0000

    points recalc script

---
+++ scratch/points_recalc.php
@@ -0,0 +1,22 @@
+<?php
+
+$mt = 14; do { $mt--; $pid = pcntl_fork(); } while ($pid > 0 && $mt > 0); if ($pid > 0) exit();
+
+require_once "../init.php";
+
+$delta = 10000000;
+$min = $delta * $mt;
+$max = $min + $delta;
+$cursor = $mdb->getCollection("killmails")->find(['killID' => ['$gte' => $min, '$lt' => $max]])->sort(['killID' => -1]);
+while ($cursor->hasNext()) {
+    $km = $cursor->next();
+    $points = Points::getKillPoints($km['killID']);
+    if ($km['zkb']['points'] != $points) {
+        //$mdb->set("oneWeek", ['killID' => $km['killID']], ['zkb.points' => $points]);
+        //$mdb->set("ninetyDays", ['killID' => $km['killID']], ['zkb.points' => $points]);
+        $mdb->set("killmails", ['killID' => $km['killID']], ['zkb.points' => $points]);
+        $diff = $points - $km['zkb']['points'];
+        if ($diff > 0) $diff = "+" . $diff;
+        echo $km['killID'] . " $points ($diff)\n";
+    }
+}
