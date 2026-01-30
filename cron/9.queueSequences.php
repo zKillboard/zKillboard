bae96ffbe3c742e6da50dd2d04aa6c20c63ae41f
cvweiss <cvweiss@gmail.com>
2026-01-30 18:06:12 +0000

    match redisq

--- cron/9.queueSequences.php
+++ cron/9.queueSequences.php
@@ -23,12 +23,22 @@ do {
     $sequence = (int) @$doc['value'];
     if ($sequence > 0) {
         $kill = clean($mdb->findDoc("killmails", ['sequence' => $sequence]));
+        $killID = $kill['killID'];
         $raw = clean($mdb->findDoc("esimails", ['killmail_id' => $kill['killID']]));
+
+        $zkb = $kill['zkb'];
+        $zkb['npc'] = @$kill['npc'];
+        $zkb['solo'] = @$kill['solo'];
+        $zkb['awox'] = @$kill['awox'];
+        $zkb['labels'] = @$kill['labels'];
+        $zkb['attackerCount'] = @$kill['attackerCount'];
+        $zkb['href'] = "$esiServer/killmails/$killID/".$zkb['hash'].'/';
+
         $doc = [
             'killmail_id' => $kill['killID'],
             'hash' => $kill['zkb']['hash'],
             'esi' => $raw,
-            'zkb' => $kill['zkb']
+            'zkb' => $zkb
         ];
         CloudFlare::r2sendArray($r2, $CF_R2_BUCKET, $doc, "ephemeral/$sequence.json", $options);
         $redis->sadd("queueCacheUrls", "https://r2z2.zkillboard.com/ephemeral/$sequence.json");
