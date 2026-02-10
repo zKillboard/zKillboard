0d893e5f10f0311a15dbed687bfffb53b230a758
cvweiss <cvweiss@gmail.com>
2026-02-10 16:59:29 +0000

    pvpfest tweaks

--- cron/9.pvpfest.php
+++ cron/9.pvpfest.php
@@ -28,6 +28,11 @@ while ($minute == date("Hi")) {
     $att = getFinalBlow($involved, $killID, $kill);
     $loc = getLoc($kill, $killID);
 
+    if (!isset($vic['characterID']) || !isset($att['characterID'])) {
+        $mdb->getCollection("queues")->deleteOne(['_id' => $row['_id']]);
+        continue;
+    }
+
     $att_vic = $att['characterID'] . "_" . $vic['characterID'];
     $att_ship_loc = $att['characterID'] . "_" . $vic['shipTypeID'] . "_" . $loc;
 
@@ -41,18 +46,18 @@ while ($minute == date("Hi")) {
         "killID" => $killID,
         "unixtime" => $unixtime
     ];
-    //echo "$att_vic $att_ship_loc $killID\n";
-    $mdb->getCollection("pvpfest")->deleteMany(['att_vic' => $att_vic, 'killID' => ['$gt' => $killID]]);
-    $mdb->getCollection("pvpfest")->deleteMany(['att_ship_loc' => $att_ship_loc, 'killID' => ['$gt' => $killID]]);
-    $count_att_vic = $mdb->getCollection("pvpfest")->countDocuments(['att_vic' => $att_vic]);
-    $count_att_ship_loc = $mdb->getCollection("pvpfest")->countDocuments(['att_ship_loc' => $att_ship_loc]);
-    if ($count_att_vic == 0 && $count_att_ship_loc == 0) {
-        $mdb->getCollection("pvpfest")->insertOne($record);
-    }
-    $mdb->getCollection("queues")->updateOne(
-        ['_id' => $row['_id']],
-        ['$set' => ['queue' => 'pvpfest-backup']]
-    );
+        //echo "$att_vic $att_ship_loc $killID\n";
+        $mdb->getCollection("pvpfest")->deleteMany(['att_vic' => $att_vic, 'killID' => ['$gt' => $killID]]);
+        $mdb->getCollection("pvpfest")->deleteMany(['att_ship_loc' => $att_ship_loc, 'killID' => ['$gt' => $killID]]);
+        $count_att_vic = $mdb->getCollection("pvpfest")->countDocuments(['att_vic' => $att_vic]);
+        $count_att_ship_loc = $mdb->getCollection("pvpfest")->countDocuments(['att_ship_loc' => $att_ship_loc]);
+        if ($count_att_vic == 0 && $count_att_ship_loc == 0) {
+            $mdb->getCollection("pvpfest")->insertOne($record);
+        }
+        $mdb->getCollection("queues")->updateOne(
+                ['_id' => $row['_id']],
+                ['$set' => ['queue' => 'pvpfest-backup']]
+                );
 
 }
 
@@ -60,7 +65,7 @@ function getFinalBlow($involved, $killID, $kill) {
     foreach ($involved as $inv) {
         if (isset($inv['finalBlow']) && $inv['finalBlow'] === true && isset($inv['characterID'])) return $inv;
     }
-	
+
     // NPC somehow got a final blow, look for the first record that has a characterID instead
     foreach ($involved as $inv) {
         if (isset($inv['characterID']) && $inv['characterID'] > 0) return $inv;
@@ -69,7 +74,7 @@ function getFinalBlow($involved, $killID, $kill) {
 }
 
 function getLoc($kill, $killID) {
-	if ($kill['system']['solarSystemID'] === 30100000) return "loc:nullsec";
+    if ($kill['system']['solarSystemID'] === 30100000) return "loc:nullsec";
     if ($kill['system']['regionID'] === 10000070) return "loc:pochven";
 
     $labels = $kill['labels'];
@@ -77,5 +82,5 @@ function getLoc($kill, $killID) {
         if (substr($label, 0, 4) == "loc:") return $label;
     }
 
-	throw new Exception("no location?! $killID");
+    throw new Exception("no location?! $killID");
 }
