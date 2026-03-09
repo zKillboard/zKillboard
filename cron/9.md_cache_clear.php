cfb2a4be10570aee7e331655f7cdeafcf9dff251
cvweiss <cvweiss@gmail.com>
2026-03-09 13:00:46 +0000

    clear images too

--- cron/9.md_cache_clear.php
+++ cron/9.md_cache_clear.php
@@ -6,7 +6,10 @@ $key = "md:special";
 
 $storedSpecial = $kvc->get($key);
 if ($special != $storedSpecial) {
+    sleep(30);
     Util::out("MarkeeDragon sale has started, ended, or has been modified...");
+    $redis->sadd("queueCacheUrls", "https://zkillboard.com/img/mdpromo1.png");
+    $redis->sadd("queueCacheUrls", "https://zkillboard.com/img/mdpromo2.png");
     $redis->sadd("queueCacheTags", "detail");
     $redis->sadd("queueCacheTags", "overview");
     $kvc->set($key, $special);
