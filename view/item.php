2c4e95215e75cbb5f82c9f1cdc1e72e198ea8665
cvweiss <cvweiss@gmail.com>
2026-03-28 17:48:51 +0000

    corrected a really odd query

--- view/item.php
+++ view/item.php
@@ -19,8 +19,8 @@ function handler($request, $response, $args, $container) {
     $info['description'] = strip_tags(@$info['description']);
     $info['price'] = Price::getItemPrice($id, date('Ymd'));
 
-    $cursor = $mdb->find('killmails', ['involved.shipTypeID' => (int) $id], [], 1);
-    $hasKills = (iterator_count($cursor) > 0);
+    $doc = $mdb->findDoc('killmails', ['involved.shipTypeID' => (int) $id]);
+    $hasKills = $doc !== null;
 
     $info['attributes'] = array();
 
