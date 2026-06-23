e820fc946d27f158341bf35f33aa2e0e72c65d43
cvweiss <cvweiss@gmail.com>
2026-06-23 11:18:27 -0400

    scoped properly, don't show empty when userid 0

--- view/favorites.php
+++ view/favorites.php
@@ -17,23 +17,25 @@ function handler($request, $response, $args, $container) {
     if ($scope == 'corporation') $title = 'Corporation Favorites';
     if ($scope == 'alliance') $title = 'Alliance Favorites';
     if (!User::isLoggedIn()) {
-        $kills = [];
-    } else {
-        $userID = (int) User::getUserID();
-        $id = $userID;
-        if ($scope != 'character') {
-            $info = Info::getInfo('characterID', $userID);
-            if ($scope == 'corporation') $id = (int) (@$info['corporationID'] ?: @$info['corporation_id']);
-            if ($scope == 'alliance') $id = (int) (@$info['allianceID'] ?: @$info['alliance_id']);
-        }
-
-        $query = ['characterID' => (int) $id];
-        if ($scope == 'corporation' && $id <= 1999999) {
-            $query = null;
-        }
-        $killIDs = $query == null ? [] : $mdb->find("favorites", $query, ['killID' => -1]);
-        $kills = Kills::getDetails($killIDs);
+        $sessID = session_id();
+        $redis->setex("forward:$sessID", 900, $_SERVER['REQUEST_URI'] ?? '/account/favorites/');
+        return $response->withStatus(302)->withHeader('Location', '/ccpoauth2/');
     }
+
+    $userID = (int) User::getUserID();
+    $id = $userID;
+    if ($scope != 'character') {
+        $info = Info::getInfo('characterID', $userID);
+        if ($scope == 'corporation') $id = (int) (@$info['corporationID'] ?: @$info['corporation_id']);
+        if ($scope == 'alliance') $id = (int) (@$info['allianceID'] ?: @$info['alliance_id']);
+    }
+
+    if (($scope == 'corporation' && $id <= 1999999) || ($scope == 'alliance' && $id <= 0)) {
+        return $response->withStatus(403)->withHeader('Cache-Tag', 'www,account,favorites');
+    }
+
+    $killIDs = $mdb->find("favorites", ['characterID' => (int) $id], ['killID' => -1]);
+    $kills = Kills::getDetails($killIDs);
 
     $data = ['kills' => $kills, 'title' => $title, 'scope' => $scope];
     return $container->get('view')->render($response->withHeader('Cache-Tag', 'www,account,favorites'), 'favorites.pug', $data);
