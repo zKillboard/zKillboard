98eb0e8b2c4b26242b341caaef4151672c707759
cvweiss <cvweiss@gmail.com>
2026-09-24 08:46:59 -0400

    some cookie tweaks

--- index.php
+++ index.php
@@ -86,6 +86,7 @@ $app = AppFactory::create();
 
 // Set up the session if we need it for this uri
 if (substr($uri, 0, 9) == "/sponsor/" || substr($uri, 0, 11) == '/crestmail/' || $uri == '/navbar/' || $uri == '/simulate/save/' || substr($uri, 0, 9) == '/account/' || $uri == '/logout/' || substr($uri, 0, 4) == '/ccp' || substr($uri, 0, 20) == "/cache/bypass/login/") {
+    session_set_cookie_params($cookie_params);
     session_set_save_handler(new MongoSessionHandler($mdb->getCollection("sessions")), true);
     session_start();
 }
